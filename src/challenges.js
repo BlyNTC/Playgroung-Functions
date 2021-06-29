dcaff7f3c79fdde395b9f561e89aae659bbff5d5
Yasmin <yasminleticia_@hotmail.com>
2021-06-29 16:38:26 -0300

    Ex 1 até o 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,68 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a, b) {
   // seu código aqui
+  if (a == 1 && b == 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let calc = (base * height) / 2;
+  return calc;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
+  let resultado = string.split(" ");
+  return resultado;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  let string = array[array.length - 1] + ', ' + array[0];
+  return string;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let total = wins * 3 + ties;
+  return total;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
+  let maior = Math.max.apply(null, array);
+  let cont = 0;
+  for (let i of array) {
+    if (i == maior) {
+      cont += 1;
+    }
+  }
+
+  return cont;
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2';
+  }
+  else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return 'cat1';
+  }
+  else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
