8bc9dbdf2caa587f6cee46e73af91a1b6880248a
Marcio <marciofonseca.ufrj@gmail.com>
2021-06-29 16:22:07 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -12,11 +12,12 @@ function compareTrue(n1,n2) {
 }
 
 
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  let area=((base*height)/2)
+  return area
 }
+
 
 // Desafio 3
 function splitSentence() {
