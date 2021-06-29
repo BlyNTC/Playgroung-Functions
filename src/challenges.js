d9881ab453e70c519c3aca4491be17924f82ab3b
tiagosathler <sathler@gmail.com>
2021-06-29 17:06:26 -0300

    Function 9: done

--- src/challenges.js
+++ src/challenges.js
@@ -97,11 +97,58 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  //string = string.toLowerCase();
+  let result = '';
+  for (char of string) {
+    switch (char) {
+      case 'a':
+        result += '1';
+        break;
+      case 'e':
+        result += '2';
+        break;
+      case 'i':
+        result += '3';
+        break;
+      case 'o':
+        result += '4';
+        break;
+      case 'u':
+        result += '5';
+        break;
+      default:
+        result += char;        
+    }
+  }
+  return result;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  //string = string.toLowerCase();
+  let result = '';
+  for (char of string) {
+    switch (char) {
+      case '1':
+        result += 'a';
+        break;
+      case '2':
+        result += 'e';
+        break;
+      case '3':
+        result += 'i';
+        break;
+      case '4':
+        result += 'o';
+        break;
+      case '5':
+        result += 'u';
+        break;
+      default:
+        result += char;        
+    }
+  }
+  return result;
 }
 
 module.exports = {
