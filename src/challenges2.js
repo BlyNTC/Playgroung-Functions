2abc9e27c29ef102b2311066a376386d2e109290
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-06-29 19:43:22 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,24 +1,65 @@
+// Funções auxiliares
+function countNumbers(array) {
+  let count = {};
+
+  for (let num of array) {
+    count[num] = num in count ? count[num] + 1 : 1;
+  }
+  return count;
+}
+
+function phoneNumberLengthCheck(array) {
+  return array.length === 11;
+}
+
+function phoneNumberRangeCheck(array) {
+  for (let num of array) {
+    if (num < 0 || num > 9) { return false; }
+  }
+  return true;
+}
+
+function phoneNumberCountCheck(array) {
+  let count = countNumbers(array);
+
+  for (let num in count) {
+    if (count[num] >= 3) { return false; }
+  }
+  return true;
+}
+
 // Desafio 10
 function techList(array, name) {
   // seu código aqui
   let list = [];
 
-  if(array.length === 0) { return 'Vazio!'};
+  if (array.length === 0) { return 'Vazio!'; }
 
   array.sort();
-  for (let str of array) { 
-
-      list.push({ 
-          tech: str,
-          name: name
-        });
+  for (let str of array) {
+    list.push({
+      tech: str,
+      name,
+    });
   }
   return list;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  if (!phoneNumberLengthCheck) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (!phoneNumberRangeCheck || !phoneNumberCountCheck) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let part1 = `(${array[0]}${array[1]}) `;
+  let part2 = `${array.slice(2, 7).join('')}-`;
+  let part3 = `${array.slice(7).join('')}`;
+
+  return part1 + part2 + part3;
 }
 
 // Desafio 12
