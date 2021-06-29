88ba03d95a459cb2cf86086a92e80ef06ec4b1ff
darthyd <luaks.skt8@gmail.com>
2021-06-29 15:51:14 -0300

    challenge 11: generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,8 +14,20 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let qtd = array.reduce((acumulador, elemento) => {
+    acumulador[elemento] = (elemento in acumulador ? acumulador[elemento] + 1 : 1);
+    return acumulador;
+  }, {});
+
+  if (array.length !== 11) return 'Array com tamanho incorreto.';
+  if (array.some((e) => e < 0 || e > 9 || qtd[e] >= 3)) return 'não é possível gerar um número de telefone com esses valores';
+
+  const op = array.splice(0, 2).join('');
+  const part1 = array.splice(0, 5).join('');
+  const tel = `(${op}) ${part1}-${array.join('')}`;
+
+  return tel;
 }
 
 // Desafio 12
