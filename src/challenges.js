8dc4bb97256f31037efdc212c8f4b92ccaaae403
Bruno Amoretti Simalha Guimarães <brunosimalha@gmail.com>
2021-06-29 16:43:08 -0300

    Resolve desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,13 +9,13 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return (string.split(' '));
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+
 }
 
 // Desafio 5
