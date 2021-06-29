fd363151eb2e120a6c5b0bef6702416977aabe1a
Marcio <marciofonseca.ufrj@gmail.com>
2021-06-29 19:47:49 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -59,12 +59,21 @@ function highestCount(arrayNumeros) {
     }
   return contador
 }
-console.log(highestCount([1, 2, 9, 9, 9]))
+
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+  if((Math.abs(cat1-mouse))<(Math.abs(cat2-mouse))){
+    return('cat1')
+  }else if((Math.abs(cat2-mouse))<(Math.abs(cat1-mouse))){
+    return('cat2')
+  }else if((Math.abs(cat1-mouse))===(Math.abs(cat2-mouse))){
+    return('os gatos trombam e o rato foge')
+  }
 }
+
+
+
 
 // Desafio 8
 function fizzBuzz() {
