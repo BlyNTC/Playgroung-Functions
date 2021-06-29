c82b96ceb0560259ae300c70e87f1dd571c690ff
MuriloM <muriloommaia@gmail.com>
2021-06-29 16:20:17 -0300

    9-encode

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,8 @@
 function compareTrue(valor1, valor2) {
   if (valor1 && valor2) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
@@ -76,11 +75,55 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let array = string.split('');
+  for (let i in array) {
+    switch (array[i]) {
+      case 'a':
+        array[i]="1";
+        break;
+      case 'e':
+        array[i] = "2";
+        break;
+      case 'i':
+        array[i] = "3";
+        break;
+      case 'o':
+        array[i] = "4";
+        break;
+      case 'u':
+        array[i] = "5";
+        break;
+        
+    }
+  }
+  array = array.join("");
+  return array;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let array = string.split('');
+  for (let i in array) {
+    switch (array[i]) {
+      case '1':
+        array[i]="a";
+        break;
+      case '2':
+        array[i] = "e";
+        break;
+      case '3':
+        array[i] = "i";
+        break;
+      case '4':
+        array[i] = "o";
+        break;
+      case '5':
+        array[i] = "u";
+        break;
+        
+    }
+  }
+  array = array.join("");
+  return array;
 }
 
 module.exports = {
