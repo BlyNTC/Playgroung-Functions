8c0e458c99b94ddca9c7853027321bc90be6b090
unamednada <dias.gustavo.dias@gmail.com>
2021-06-29 18:18:49 -0300

    Add reference code

--- src/challenges2.js
+++ src/challenges2.js
@@ -47,7 +47,7 @@ function generatePhoneNumber(array) {
   return `(${number.slice(0, 2)}) ${number.slice(2, 7)}-${number.slice(7)}`;
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
