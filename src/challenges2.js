d2627ed8eafabdd898c200335af9d7756943e91b
Luan Peres <delinoperes@gmail.com>
2021-06-29 17:09:31 -0300

    Desafio 13 corrigido

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,29 +18,36 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  if (arr.length != 11) return "Array com tamanho incorreto.";
+
+  arr.forEach(el => {
+    if (el < 0 || el > 9) return "não é possível gerar um número de telefone com esses valores"
+  });
+
+  let maskedPhone = $`(${arr[0]}${arr[1]}) ${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`
+  return maskedPhone
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if ((lineA + lineB) > lineC) {
     if ((lineA + lineC) > lineB) {
-      if ((lineC + lineB) > lineA) return true
+      if ((lineC + lineB) > lineA) return true;
     }
   }
-  return false
+  return false;
 }
 
 // Desafio 13
 function hydrate(s) {
-  let num = s.match(/\d+/g) //Busque por todos os digitos (\d+) e os agrupe (g)
+  let num = s.match(/\d+/g);
   let sum = 0;
   
-  num.forEach(el => sum += Number(el))
+  num.forEach(el => sum += Number(el));
 
-  if (num === 1) return sum + " copo de água"
-  return sum + " copos de água"
+  if (num == 1) return sum + " copo de água";
+  return sum + " copos de água";
 }
 
 module.exports = {
