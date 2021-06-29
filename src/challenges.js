783b3db7860dacc24a7e307827c3a8ecab118fda
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-06-29 19:25:24 -0300

    requisito 9 - funcao que codifique e decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -16,7 +16,7 @@ function splitSentence(phrase) {
 // Desafio 4
 function concatName(array) {
   let size = array.length;
-  let concatenation = array[size - 1] + ', '  + array[0];
+  let concatenation = array[size - 1] + ', ' + array[0];
   return concatenation;
 }
 
@@ -64,11 +64,11 @@ function fizzBuzz(array) {
   for (let num of array) {
     let div3 = num % 3;
     let div5 = num % 5;
-    if (div3 === 0 && div5 !== 0) {
+    if (div3 === 0 && div5 === 0) {
       newArray.push('fizz');
-    } else if (div5 === 0 && div3 !== 0) {
+    } else if (div5 === 0) {
       newArray.push('buzz');
-    } else if (div5 === 0 && div3 === 0) {
+    } else if (div3 === 0) {
       newArray.push('fizzBuzz');
     } else {
       newArray.push('bug!');
@@ -78,11 +78,56 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let newPhrase = '';
+  for (let i = 0; i < phrase.length; i += 1) {
+    switch(phrase[i]){
+      case 'a':
+        newPhrase += '1';
+        break;
+      case 'e':
+        newPhrase += '2';
+        break;
+      case 'i':
+        newPhrase += '3';
+        break;
+      case 'o':
+        newPhrase += '4';
+        break;
+      case 'u':
+        newPhrase += '5';
+        break;
+      default:
+        newPhrase += phrase[i];
+    }
+  }
+  return newPhrase;
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase) {
+  let newPhrase = '';
+  for (let i = 0; i < phrase.length; i += 1) {
+    switch(phrase[i]){
+      case '1':
+        newPhrase += 'a';
+        break;
+      case '2':
+        newPhrase += 'e';
+        break;
+      case '3':
+        newPhrase += 'i';
+        break;
+      case '4':
+        newPhrase += 'o';
+        break;
+      case '5':
+        newPhrase += 'u';
+        break;
+      default:
+        newPhrase += phrase[i];
+    }
+  }
+  return newPhrase;
 }
 
 module.exports = {
