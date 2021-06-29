ff360576e01aeb9b6b133fb6e6dcdec718ad62c9
Amanda Zotelli <amzotelli@gmail.com>
2021-06-29 17:32:42 -0300

    requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,11 @@
 // Desafio 1
 function compareTrue() {
-  if ((a === true) && (b === true)) {
-  return true;
-  } else {
-  return false;
-  }
+  
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height) / 2;
 }
 
 // Desafio 3
