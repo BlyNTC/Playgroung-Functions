69e38125a8cf8450f151d899fda5fb42e5409f0d
Pedro Figueiredo <pedrofigueiredo25@outlook.com>
2021-06-29 17:34:00 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -6,14 +6,12 @@ function compareTrue(param1, param2) {
     return false;
   }
 }
-compareTrue()
 
 // Desafio 2
 function calcArea(base, height) {
   area = ((base * height) / 2);
   return area;
 }
-calcArea(51, 1);
 
 // Desafio 3 - material consultado para resolução https://www.w3schools.com/jsref/jsref_split.asp
 function splitSentence(stringParam) {
@@ -21,24 +19,21 @@ function splitSentence(stringParam) {
   return newArray
 }
 
-splitSentence('go Trybe');
-
 // Desafio 4
 function concatName(paramArray) {
-  let primeiroIndice = paramArray [0]
-  let ultimoIndice = paramArray[paramArray.lenght -1]
+  let primeiroIndice = paramArray[0];
+  let ultimoIndice = paramArray[paramArray.length - 1];
+  return ultimoIndice + ', ' + primeiroIndice;
 }
-
-concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let pontosVitoria = 3;
   let pontosEmpate = 1;
   pontosTotais = (wins * pontosVitoria) + (ties * pontosEmpate);
-  return pontosTotais; 
+  return pontosTotais;
 }
-footballPoints()
+footballPoints();
 
 // Desafio 6
 function highestCount() {
