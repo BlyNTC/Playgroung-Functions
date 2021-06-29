daf90092d009bc630880e0b3bac1cd5b582fe02c
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-29 19:59:31 -0300

    Desafio 11 - Bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,21 +4,49 @@ function techList(technologies, name) {
 
   if (technologies.length === 0) {
     return 'Vazio!';
-  } else {
-    for (let value of technologies.sort()) {
-      let object = {}
-      object.tech = value;
-      object.name = name;
-      output.push(object);
-    }
+  }
+
+  for (let value of technologies.sort()) {
+    let object = {};
+    object.tech = value;
+    object.name = name;
+    output.push(object);
   }
 
   return output;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  let numberCount = {};
+
+  for (let value of numbers) { // count how many times the value appears
+    if (numberCount[value] === undefined) {
+      numberCount[value] = 1;
+    } else {
+      numberCount[value] += 1;
+    }
+  }
+
+  let repeated = 0;
+
+  for (let count in numberCount) { // determines how many times the value appears
+    if (numberCount[count] > repeated) {
+      repeated = numberCount[count];
+    }
+  }
+
+  for (let value of numbers) {
+    if (value < 0 || value > 9 || repeated >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  return numbers.join('').replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3'); // Beseado no Exemplo 4 em <https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176>
 }
 
 // Desafio 12
