40bfc5c3463a8494f5228c1b772f1a8a322ed2ad
rodPinheiro <rod_bma@hotmail.com>
2021-06-29 16:27:06 -0300

    Desafio 9 PASSED

--- src/challenges.js
+++ src/challenges.js
@@ -81,12 +81,56 @@ function fizzBuzz(arr) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  let arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "a":
+        arr[i] = "1"
+        break;
+      case "e":
+        arr[i] = "2"
+        break;
+      case "i":
+        arr[i] = "3"
+        break;
+      case "o":
+        arr[i] = "4"
+        break;
+      case "u":
+        arr[i] = "5"
+        break;
+      default:
+        break;
+    }
+  }
+  return arr.join("")
 }
 
-function decode() {
-  // seu código aqui
+function decode(str) {
+  let arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "1":
+        arr[i] = "a"
+        break;
+      case "2":
+        arr[i] = "e"
+        break;
+      case "3":
+        arr[i] = "i"
+        break;
+      case "4":
+        arr[i] = "o"
+        break;
+      case "5":
+        arr[i] = "u"
+        break;
+      default:
+        break;
+    }
+  }
+  return arr.join("")
 }
 
 module.exports = {
