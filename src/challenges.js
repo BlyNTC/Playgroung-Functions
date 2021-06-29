d6224f1c0829cdcaffcab899c77017831c222c39
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-29 16:46:03 -0300

    Add exercise 9

--- src/challenges.js
+++ src/challenges.js
@@ -66,11 +66,36 @@ function fizzBuzz(array) {
 };
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  const encoder = {
+    a : '1',
+    e : '2',
+    i : '3',
+    o : '4',
+    u : '5'
+  };
+  let result = "";
+  for(let character of string){
+    if(character in encoder) result += encoder[character];
+    else result += character;
+  };
+  return result;
 };
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  const decoder = {
+    1 : 'a',
+    2 : 'e',
+    3 : 'i',
+    4 : 'o',
+    5 : 'u'
+  };
+  let result = "";
+  for(let character of string){
+    if(character in decoder) result += decoder[character];
+    else result += character;
+  };
+  return result;
 };
 
 module.exports = {
