4d7ed87007141f844270de66b9e878bdeb545a63
WalmirLucena <walmirlucena3@gmail.com>
2021-06-29 16:17:33 -0400

    Adiciona questoes de 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,69 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a,b) {
   // seu código aqui
+  if(a === true && b === true){
+    return true;
+  } else{
+    return false;
+  }
+
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, altura) {
   // seu código aqui
+  let area = (base*altura)/2;
+  return area;
 }
 
+
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
+  let array = [''];
+  let cont=0;
+ 
+
+  for(let index=0; index < string.length ; index +=1){
+    let indicaEspaco = 0;
+      if(string[index] === " "){
+      cont +=1;
+      indicaEspaco +=1;
+      } 
+      if(indicaEspaco === 0 ){
+      array[cont]= array[cont] + string[index];
+      } else{
+        array[cont]='';
+      }
+    }
+return array;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  let string= '';
+  let tam = array.length;
+  string= array[tam-1];
+  string= string + ", " + array[0];
+  
+ console.log(string);
+ return string;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let soma = 0;
+  soma = 3*wins + 1*ties;
+  return soma;
+
 }
 
 // Desafio 6
 function highestCount() {
   // seu código aqui
+  
 }
 
 // Desafio 7
