fc88eb7d153a3d233a9ea991077b1356e354f128
WalmirLucena <walmirlucena3@gmail.com>
2021-06-29 18:16:45 -0400

    Adiciona questao 9

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,9 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a, b) {
   // seu código aqui
-  if(a === true && b === true){
+  if (a === true && b === true) {
     return true;
-  } else{
+  } else {
     return false;
   }
 
@@ -12,7 +12,7 @@ function compareTrue(a,b) {
 // Desafio 2
 function calcArea(base, altura) {
   // seu código aqui
-  let area = (base*altura)/2;
+  let area = (base * altura) / 2;
   return area;
 }
 
@@ -21,41 +21,41 @@ function calcArea(base, altura) {
 function splitSentence(string) {
   // seu código aqui
   let array = [''];
-  let cont=0;
- 
+  let cont = 0;
 
-  for(let index=0; index < string.length ; index +=1){
+
+  for (let index = 0; index < string.length; index += 1) {
     let indicaEspaco = 0;
-      if(string[index] === " "){
-      cont +=1;
-      indicaEspaco +=1;
-      } 
-      if(indicaEspaco === 0 ){
-      array[cont]= array[cont] + string[index];
-      } else{
-        array[cont]='';
-      }
+    if (string[index] === " ") {
+      cont += 1;
+      indicaEspaco += 1;
     }
-return array;
+    if (indicaEspaco === 0) {
+      array[cont] = array[cont] + string[index];
+    } else {
+      array[cont] = '';
+    }
+  }
+  return array;
 }
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  let string= '';
+  let string = '';
   let tam = array.length;
-  string= array[tam-1];
-  string= string + ", " + array[0];
-  
- console.log(string);
- return string;
+  string = array[tam - 1];
+  string = string + ", " + array[0];
+
+  console.log(string);
+  return string;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let soma = 0;
-  soma = 3*wins + 1*ties;
+  soma = 3 * wins + 1 * ties;
   return soma;
 
 }
@@ -63,25 +63,25 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(array) {
   // seu código aqui
-  let contRepete =0;
-  let contNumero =0;
-  
-  for(let key in array){
-    let verifica = array[key];
-      for(let index in array){
-        if(verifica=== array[index]){
-          contNumero += 1;
-          
-        }
-       
-      }
-      if(contNumero > contRepete ){
-        contRepete = contNumero;
-        
+  let contRepete = 0;
+  let contNumero = 0;
 
-      } 
-      contNumero=0;
-      
+  for (let key in array) {
+    let verifica = array[key];
+    for (let index in array) {
+      if (verifica === array[index]) {
+        contNumero += 1;
+
+      }
+
+    }
+    if (contNumero > contRepete) {
+      contRepete = contNumero;
+
+
+    }
+    contNumero = 0;
+
 
   }
   return contRepete;
@@ -89,14 +89,14 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse(cat1 , cat2) {
+function catAndMouse(cat1, cat2) {
   // seu código aqui
   // cat1 é a distancia em unidades do gato1 para o rato
-  if(cat1>cat2){
-    return 'cat1';
-  } else if(cat2>cat1){
+  if (cat1 > cat2) {
     return 'cat2';
-  } else{
+  } else if (cat2 > cat1) {
+    return 'cat1';
+  } else {
     return "os gatos trombam e o rato foge";
   }
 
@@ -105,32 +105,72 @@ function catAndMouse(cat1 , cat2) {
 // Desafio 8
 function fizzBuzz(array) {
   // seu código aqui
-  let fizz ='fizz';
+  let fizz = 'fizz';
   let buzz = 'buzz';
   let fizzBuzz = 'fizzBuzz';
-  let bug ='bug';
-  let lista=[''];
+  let bug = 'bug';
+  let lista = [''];
 
-  for (let key in array){
-    if( array[key]%5===0 && array[key]%3===0){
-      lista[key]= fizzBuzz;
-    } else if(array[key]%5===0){
-      lista[key]= buzz;
-    } else if(array[key]%3===0){
-      lista[key]= fizz;         
+  for (let key in array) {
+    if (array[key] % 5 === 0 && array[key] % 3 === 0) {
+      lista[key] = fizzBuzz;
+    } else if (array[key] % 5 === 0) {
+      lista[key] = buzz;
+    } else if (array[key] % 3 === 0) {
+      lista[key] = fizz;
     } else {
-      lista[key]= bug;
+      lista[key] = bug;
     }
+  }
+  return lista;
 }
-return lista;
-}
+
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  let frase = '';
+  for (let value of string) {
+    if (value === "a") {
+      frase += 1;
+    } else if (value === "e") {
+      frase += 2;
+    } else if (value === "i") {
+      frase += 3;
+    } else if (value === "o") {
+      frase += 4;
+    } else if (value === "u") {
+      frase += 5;
+    } else {
+      frase += value;
+    }
+  }
+  return frase;
+
 }
-function decode() {
+
+
+function decode(frase2) {
   // seu código aqui
+  let frase = '';
+  for (let value of frase2) {
+    if (value === 1) {
+      frase += 'a';
+    } else if (value === 2) {
+      frase += 'e';
+    } else if (value === 3) {
+      frase += 'i';
+    } else if (value === 4) {
+      frase += 'o';
+    } else if (value === 5) {
+      frase += 'u';
+    } else {
+      frase += value;
+    }
+  }
+  return frase;
+
 }
+
 
 module.exports = {
   calcArea,
