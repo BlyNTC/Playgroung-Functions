896c70aa5218e57edf811af93065411657b24e71
guiroos <guilherme.roosr@gmail.com>
2021-06-29 15:47:01 -0300

    Desafio 3 completo

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  splittedSentence = sentence.split(' ')
+  return splittedSentence;
 }
 
 // Desafio 4
