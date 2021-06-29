e13f19a7bb01e1c794a4e0c0f278d1812092e12c
Caio César Amorim <caiocavalcante063@gmail.com>
2021-06-29 17:05:42 -0300

    resolve desafio1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(verdade1, verdade2) {
   // seu código aqui
+  if (verdade1 && verdade2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
