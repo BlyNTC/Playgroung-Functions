268d61ae7806d7283d26e1978aac7c4e8e533923
Gustavo Dolzan <gudolzan@gmail.com>
2021-06-29 16:53:17 -0300

    Desafios 7 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,18 @@
 // Desafio 1
 function compareTrue(bool1, bool2) {
+  let output;
   if (bool1 === true && bool2 === true) {
-    return true;
+    output = true;
   } else {
-    return false;
+    output = false;
   }
+  return output;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return base * height / 2;
+  let area = base * height / 2;
+  return area;
 }
 
 // Desafio 3
@@ -19,12 +22,14 @@ function splitSentence(sentence) {
 
 // Desafio 4
 function concatName(name) {
-  return name[name.length - 1] + ', ' + name[0];
+  let concatName = name[name.length - 1] + ', ' + name[0];
+  return concatName;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return wins * 3 + ties;
+  let points = wins * 3 + ties;
+  return points;
 }
 
 // Desafio 6
@@ -33,14 +38,40 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let output;
+  let distance1 = Math.abs(mouse - cat1),
+    distance2 = Math.abs(mouse - cat2);
+
+  if (distance1 === distance2) {
+    output = "os gatos trombam e o rato foge";
+  } else {
+    if (distance1 < distance2) {
+      output = 'cat1';
+    } else {
+      output = 'cat2';
+    }
+  }
+  return output;
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let output = [];
+  for (let number of numbers) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      output.push("fizzBuzz");
+    } else if (number % 3 === 0) {
+      output.push("fizz");
+    } else if (number % 5 === 0) {
+      output.push("buzz");
+    } else {
+      output.push("bug!");
+    };
+  };
+  return output;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
