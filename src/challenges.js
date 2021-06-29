61002412552367ac2592e830fb1e1398f680b958
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-06-29 16:31:05 -0300

    requisito 3 - funcao que divide frase

--- src/challenges.js
+++ src/challenges.js
@@ -5,12 +5,12 @@ function compareTrue(param1, param2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  return phrase.split(' ');
 }
 
 // Desafio 4
