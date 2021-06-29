170bc66e8c056be56c274f183d55d6ff6b6158ba
MartinSilva <martinempreendedor@gmail.com>
2021-06-29 17:56:32 -0300

    função generatePhonoNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,8 +13,36 @@ function techList(arrayT, oneName) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayN) {
+  if (arrayN.length === 11) {
+    let umNumero = true;
+    let number = null;
+    let counting = null;
+    let arrayNew = [...arrayN]
+    for (let valor of arrayNew.sort()) {
+      if (!umNumero) {
+        break;
+      } else if (valor > 9 || valor < 0) {
+        umNumero = false;
+      } else if (counting === 3) {
+        umNumero = false;
+      } else if (number < valor || number === null) {
+        counting = 1;
+        number = valor;
+      } else if (number === valor) {
+        counting += 1;
+
+      }
+    }
+    
+    if (!umNumero) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    else return `(${arrayN[0]}${arrayN[1]}) ${arrayN[2]}${arrayN[3]}${arrayN[4]}${arrayN[5]}${arrayN[6]}-${arrayN[7]}${arrayN[8]}${arrayN[9]}${arrayN[10]}`;
+    
+  } 
+  else return "Array com tamanho incorreto.";
+
 }
 
 // Desafio 12
