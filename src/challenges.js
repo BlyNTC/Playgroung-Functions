ac633c74f014ff34147cc6d323161b8bb32a26f7
Riquelme <riquelmebandeira1@outlook.com>
2021-06-29 17:37:43 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -20,8 +20,9 @@ function splitSentence(frase) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let palavras = array[array.length-1] + ", " + array[0];
+  return palavras
 }
 
 // Desafio 5
