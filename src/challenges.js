33071aba3199f080b03b257d19f0dc15c5e98bb8
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-06-29 16:34:41 -0300

    Desafio 1 refeito mais bonito

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,7 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  if (valor1 && valor2) {
-    return true;
-  } else {
-    return false;
-  }
+  valor1 && valor2;
+  return valor1 && valor2;
 }
 
 // Desafio 2
