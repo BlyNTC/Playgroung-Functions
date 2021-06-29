f359a3b85470b78aa87b2465615bcb4fcbd887e1
jagrunewald <je_grunewald@hotmail.com>
2021-06-29 18:31:42 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -31,8 +31,9 @@ function concatName(nomes) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
