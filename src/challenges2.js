06837d2842ffb73c378c09fbc3a5d262457bee70
rodPinheiro <rod_bma@hotmail.com>
2021-06-29 18:00:04 -0300

    Desafio 10 PASSED

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,33 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arr, name) {
+  let result = [];
+  if (arr.length === 0) {
+    return "Vazio!"
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      var obj = {
+        tech: arr[i],
+        name: name
+      }
+      result.push(obj);
+    }
+  }
+  console.log(result);
+  //Funcao compare para o metodo sort adaptada do link https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+  function compare(a, b) {
+
+    const techA = a.tech;
+    const techB = b.tech;
+
+    let comparison = 0;
+    if (techA > techB) {
+      comparison = 1;
+    } else if (techA < techB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  return result.sort(compare);
 }
 
 // Desafio 11
