48e8e2c8faf83daf148a4b3326e65c1a0d07a4e2
Williamns <williamnstolb@gmail.com>
2021-06-29 16:09:40 -0300

    Desafio 2 completo

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,12 @@ function compareTrue(p1, p2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = 0;
+
+  area = (base*height)/2;
+
+  return area;
 }
 
 // Desafio 3
