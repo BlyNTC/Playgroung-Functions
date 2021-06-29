0165ad28c19df8559b6e381357b79e47ffe94734
Guinxard <lucasmfarias3@gmail.com>
2021-06-29 15:37:51 -0300

    Requisito 1, 2 e 3 ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,19 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(bool1, bool2) {
+  comp = bool1 && bool2;
+  return comp;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height)/2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let split = frase.split(' ');
+  return split;
 }
 
 // Desafio 4
