4f27bc74bc82a999cb5cba4b2175c37eaef3f478
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-06-29 16:31:01 -0300

    Exercicio 3 - separando as frases dentro de um array

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,11 @@ function calcArea(base, height) {
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let palavra;
+  palavra = frase.split(" ");
+
+  return palavra;
 }
 
 // Desafio 4
