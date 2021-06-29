4bb4b1ecc47339f368af9f2a2e65fdc0d9acb968
Marco <marcoantonio99d@gmail.com>
2021-06-29 15:46:01 -0300

    exercicio 7 pausado

--- src/challenges.js
+++ src/challenges.js
@@ -46,8 +46,17 @@ for (let index2 = 0; index2 < nums.length; index2 += 1) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distancia1 = (mouse) - (cat1);
+  let distancia2 = (mouse) - (cat2);
+
+  if (distancia1 > distancia2) {
+      return "cat2"
+  } else if (distancia2 > distancia1) {
+      return "cat1"
+  } else if (distancia1 == distancia2) {
+      return ("os gatos trombam e o rato foge")
+  }
 }
 
 // Desafio 8
