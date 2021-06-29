e53eeb2bf4025f9ae1f9ee39c55de171664b762f
Filipe Brochier <filipe.brochier@gmail.com>
2021-06-29 15:54:11 -0300

    Desafio 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(x, y) {
+  if (x && y === true){
+    return true;
+    } else {
+      return false;
+    }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+  function calcArea(base, height) {
+    let area = 0;
+    area = (base * height) / 2;
+    return area; 
 }
 
 // Desafio 3
