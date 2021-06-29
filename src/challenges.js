2478baf12bc54d1f37b6d9a3935efaae4da5fb65
MartinSilva <martinempreendedor@gmail.com>
2021-06-29 16:26:00 -0300

    refatoração para limpeza de codigo

--- src/challenges.js
+++ src/challenges.js
@@ -6,13 +6,13 @@ function compareTrue(valor1, valor2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return base * height / 2;
+  return (base * height) / 2;
   // seu código aqui
 }
 
 // Desafio 3
 function splitSentence(stringSplitting) {
-  return stringSplitting.split(" ");
+  return stringSplitting.split(' ');
   // seu código aqui
 }
 
@@ -30,22 +30,17 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(arrayNumber) {
-
   let higherNumber = null;
   let counting = null;
   for (let index = 0; index < arrayNumber.length; index += 1) {
-
     if (higherNumber < arrayNumber[index] || higherNumber === null) {
       counting = 1;
       higherNumber = arrayNumber[index];
-    } else if (higherNumber === arrayNumber[index]) {
+    } 
+    else if (higherNumber === arrayNumber[index]) {
       counting += 1;
-    } else {
-      continue
-    }
-
+    } 
   }
-
   return counting; // seu código aqui
 }
 
@@ -68,14 +63,14 @@ function fizzBuzz(arrayNumberFB) {
   for (let index = 0; index < arrayNumberFB.length; index += 1) {
     if (arrayNumberFB[index] % 3 === 0) {
       if (arrayNumberFB[index] % 5 === 0) {
-        arrayFB.push("fizzBuzz")
+        arrayFB.push('fizzBuzz')
       } else {
-        arrayFB.push("fizz")
+        arrayFB.push('fizz')
       }
     } else if (arrayNumberFB[index] % 5 === 0) {
-      arrayFB.push("buzz")
+      arrayFB.push('buzz')
     } else {
-      arrayFB.push("bug!")
+      arrayFB.push('bug!')
     }
   }
   // seu código aqui
@@ -87,16 +82,16 @@ function fizzBuzz(arrayNumberFB) {
 function encode(eCode) {
   let nECode = ""
   for (let index = 0; index < eCode.length; index += 1) {
-    if ("a" === eCode[index]) {
-      nECode += "1"
-    } else if ("e" === eCode[index]) {
-      nECode += "2"
-    } else if ("o" === eCode[index]) {
-      nECode += "4"
-    } else if ("i" === eCode[index]) {
-      nECode += "3"
-    } else if ("u" === eCode[index]) {
-      nECode += "5"
+    if ('a' === eCode[index]) {
+      nECode += '1'
+    } else if ('e' === eCode[index]) {
+      nECode += '2'
+    } else if ('o' === eCode[index]) {
+      nECode += '4'
+    } else if ('i' === eCode[index]) {
+      nECode += '3'
+    } else if ('u' === eCode[index]) {
+      nECode += '5'
     } else nECode += eCode[index];
   }
   return nECode;
@@ -105,16 +100,16 @@ function encode(eCode) {
 function decode(dCode) {
   let nDCode = ""
   for (let index = 0; index < dCode.length; index += 1) {
-    if ("1" === dCode[index]) {
-      nDCode += "a"
-    } else if ("2" === dCode[index]) {
-      nDCode += "e"
-    } else if ("3" === dCode[index]) {
-      nDCode += "i"
-    } else if ("4" === dCode[index]) {
-      nDCode += "o"
-    } else if ("5" === dCode[index]) {
-      nDCode += "u"
+    if ('1' === dCode[index]) {
+      nDCode += 'a'
+    } else if ('2' === dCode[index]) {
+      nDCode += 'e'
+    } else if ('3' === dCode[index]) {
+      nDCode += 'i'
+    } else if ('4' === dCode[index]) {
+      nDCode += 'o'
+    } else if ('5' === dCode[index]) {
+      nDCode += 'u'
     } else nDCode += dCode[index];
 
 
