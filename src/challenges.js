29f56cf83fb9822646ed60f6ffeeb8bbf640a766
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-06-29 18:25:31 -0300

    exercicoo 7 e 9

--- src/challenges.js
+++ src/challenges.js
@@ -61,20 +61,27 @@ function highestCount(array) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return "cat1";
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return "cat2";
+  } else if (Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)) {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   // seu código aqui
   let frases = [];
-  for (i = 0; i < array.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     if (array[i] % 3 === 0 && array[i] % 5 === 0) {
       frases.push('fizzBuzz');
-    } else if (array[i] % 5 === 0){
+    } else if (array[i] % 5 === 0) {
       frases.push('buzz');
-    } else if (array[i] % 3 === 0){
+    } else if (array[i] % 3 === 0) {
       frases.push('fizz'); 
-    } else{
+    } else {
       frases.push('bug!');
     }
   }
@@ -82,11 +89,23 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  string = string.replace(/a/g, '1');
+  string = string.replace(/e/g, '2');
+  string = string.replace(/i/g, '3');
+  string = string.replace(/o/g, '4');
+  return string.replace(/u/g, '5');
+ 
 }
-function decode() {
+
+function decode(string2) {
   // seu código aqui
+  string2 = string2.replace(/1/g, 'a');
+  string2 = string2.replace(/2/g, 'e');
+  string2 = string2.replace(/3/g, 'i');
+  string2 = string2.replace(/4/g, 'o');
+  return string2.replace(/5/g, 'u');
 }
 
 module.exports = {
