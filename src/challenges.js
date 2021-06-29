192bbc539427eec3778cc93c4e855523042a9923
Carla <carladmaximo@gmail.com>
2021-06-29 16:09:27 -0300

    Desafios 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,16 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1,valor2) {
+  if (valor1 == true && valor2 == true){
+    return true
+  } else {
+    return false
+  } 
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  var area = (base*height)/2
+  return area
 }
 
 // Desafio 3
