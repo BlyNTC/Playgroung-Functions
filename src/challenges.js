9c0468677863ae80792939b6b833d6f5a4c8617d
Carla <carladmaximo@gmail.com>
2021-06-29 19:22:34 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,8 @@ function splitSentence(phraseSplit) {
 
 // Desafio 4
 function concatName(nome) {
-  return nome[0]+','+' '+ nome[n - 1]
+  n = nome.length
+  return nome[n - 1]+','+' '+nome[0]
 }
 
 // Desafio 5
@@ -49,26 +50,21 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  let palavras = {}; //Cria um array para receber as palavras conforme a divisibilidade do número
-  let palavra = 0 //Cria a variável palavra para ser alterada e adicionada no array palavras conforme o andamento do código
+function fizzBuzz(numeros) {
+  let palavras = []; //Cria um array para receber as palavras conforme a divisibilidade do número
   for (numero of numeros) { //Comando que faz o código analisar elemento por elemento do array recebido
     switch (numeros) { // Comando que configura a palavra recebida conforme a divisibilidade do elemento analisado e adiciona ao array palavras
-      case numero % 3 === 0:
-        palavra = 'fizz'
-        palavras.palavra.push()
+      case (numero % 3 === 0) && (numero % 5 === 0):
+        palavras.push('fizzBuzz')
         break
       case numero % 5 === 0:
-        palavra = 'buzz'
-        palavras.palavra.push()
+        palavras.push('buzz')
         break
-      case (numero % 3 === 0) && (numero % 5 === 0):
-        palavra = 'fizzBuzz'
-        palavras.palavra.push() 
+      case numero % 3 === 0:
+        palavras.palavra.push('fizz') 
         break
       default:
-        palavra = 'bug!'
-        palavras.palavra.push()
+        palavras.push('bug!')
         break
     }
   return palavras
