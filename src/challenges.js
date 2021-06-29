49a5396af674817741c1aba9c4b9f26272ed0a48
Écio Ferraz <ecioferraz@hotmail.com>
2021-06-29 16:50:35 -0300

    requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,35 @@
 // Desafio 1
-function compareTrue(num1,num2) {
+function compareTrue(num1, num2) {
   if (num1 > 0 && num2 > 0) {
     return true;
   } else {
     return false;
-  };
+  }
 }
 
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base, height) {
   let area = (base * height) / 2;
   return area;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
 }
-
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalWins = 0;
+  for (let indexWins = 0; indexWins <= wins; indexWins += 1) {
+    totalWins = wins * 3;
+  }
+  totalPoints = totalWins + ties;
+  return totalPoints;
 }
 
 // Desafio 6
