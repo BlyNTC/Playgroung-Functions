dd2117f541fc0fcda5dcff5e35c0f7549c2b6ace
Andrea <andrealh08@gmail.com>
2021-06-29 17:48:59 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,9 +13,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let array = string.split(" ");
+  return array;
 }
+
 
 // Desafio 4
 function concatName() {
