d5abc56d1067d1bd4f510cf07410690cde6a23c4
Lucas Duarte <lucs.rkt@gmail.com>
2021-06-29 20:18:21 -0300

    desafio 11 finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,13 +14,64 @@ function techList(array, name) {
     };
 
     return newArray;
-  }
-  
+  };
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])
+function generatePhoneNumber(array) {
+  let phoneNumber = '';
+  let transition;
+  let negativo = 0;
+  let maior = 0;
+  let repete = false;
+  let obj = {
+    '0': 0,
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0,
+    '9': 0
+  }
+
+  for (let number of array) {
+    if (obj.hasOwnProperty(number)) {
+      obj[number] += 1;
+    }
+
+    if (number < 0) {
+      negativo += 1;
+    }
+
+    if (number > 9) {
+      maior += 1;
+    }
+  }
+
+  for (let key in obj) {
+    if (obj[key] >= 3) {
+      repete = true;
+      break;
+    }
+  }
+
+  if (array.length != 11) {
+    return "Array com tamanho incorreto."
+  } else if (negativo > 0 || maior > 0 || repete) {
+    return "não é possível gerar um número de telefone com esses valores"
+  } else {
+    array.splice(0, 0, '(');
+    array.splice(3, 0, ')');
+    array.splice(4, 0, ' ');
+    array.splice(10, 0, '-');
+    array = array.toString();
+    array = array.replace(/,/g, '');
+  }
+  return array;
 }
 
 // Desafio 12
