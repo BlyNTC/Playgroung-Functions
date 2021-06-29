996fa5f5ce3052f1c1144f7b03d83bc62ce2761d
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-06-29 17:36:27 -0300

    Add exercise 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,9 +12,38 @@ function techList(array, name) {
   return result;
 };
 
+console.log(generatePhoneNumber([1,1,0,4,5,6,7,8,9,2,3]))
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if(array.length === 11){
+    let cache = {};
+    for(let num of array){
+      cache[num]? cache[num] += 1 : cache[num] = 1;
+      if(num < 0 || num > 9 || cache[num] >= 3) return "não é possível gerar um número de telefone com esses valores";
+    };
+    cache = [];
+    for(let i = 11; i > 0; i -= 1){
+      let randomIndex = Math.floor(Math.random() * i);
+      cache.push(array[randomIndex]);
+      array.splice(randomIndex, 1);
+    };
+    let result = "(";
+    for(let i = 0; i < 2; i += 1){
+      result += cache[0];
+      cache.shift();
+    };
+    result += ")";
+    for(let i = 0; i < 5; i += 1){
+      result += cache[0];
+      cache.shift();
+    };
+    result += "-"
+    for(let i = 0; i < 4; i += 1){
+      result += cache[0];
+      cache.shift();
+    };
+    return result;
+  }else return "Array com tamanho incorreto.";
 };
 
 // Desafio 12
