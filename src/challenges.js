a5c2886a38ae318f19e4770fb6ce89912014751a
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-06-29 17:28:12 -0300

    completa 7º funcao

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,46 @@
 // Desafio 1
 function compareTrue(primeiro, segundo) {
   // seu código aqui
+  let result;
+
   if (primeiro && segundo) {
-    return true;
+    result = true;
   } else {
-    return false;
+    result = false;
   }
+
+  return result;
 }
 
 // Desafio 2
 function calcArea(base, heigth) {
   // seu código aqui
-  return restult = (base * heigth)/2;
+  let result = (base * heigth)/2;
+
+  return result;
 }
 
 // Desafio 3
 function splitSentence(words) {
   // seu código aqui
-  let arrayOfStrings = [];
-  return arrayOfStrings = words.split(' ');
+  let arrayOfStrings = words.split(' ');
+
+  return arrayOfStrings;
 }
 
 // Desafio 4
 function concatName(name) {
   // seu código aqui
-  return (`${name[name.length-1]}, ${name[0]}`);
+  let result = (`${name[name.length-1]}, ${name[0]}`);
+
+  return result;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let result = ((wins*3)+ties);
+
   return result;
 }
 
@@ -39,14 +49,14 @@ function highestCount(highest) {
   // seu código aqui
   let highestNumber = -Infinity, counter = 0;
 
-  for (let high of highest){
-    if (high > highestNumber){
+  for (let high of highest) {
+    if (high > highestNumber) {
       highestNumber = high;
     }
   }
 
-  for (let high of highest){
-    if (high === highestNumber){
+  for (let high of highest) {
+    if (high === highestNumber) {
       counter += 1;
     }
   }
@@ -55,8 +65,31 @@ function highestCount(highest) {
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distanceCat1, distanceCat2, result;
+
+  if (mouse < cat1) {
+    distanceCat1 = mouse - cat1;
+  } else {
+    distanceCat1 = cat1 - mouse;
+  } 
+
+  if (mouse < cat2) {
+    distanceCat2 = mouse - cat2;
+  } else {
+    distanceCat2 = cat2 - mouse;
+  } 
+
+  if (distanceCat1 > distanceCat2) {
+    result = "cat1";
+  } else if (distanceCat1 == distanceCat2) {
+    result = "os gatos trombam e o rato foge";
+  } else {
+    result = "cat2"
+  }
+
+  return result;
 }
 
 // Desafio 8
