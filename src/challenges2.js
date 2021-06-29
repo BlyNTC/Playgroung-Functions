b410447df1b12ea03d6dc826b303bd6cfdd18184
tiagosathler <sathler@gmail.com>
2021-06-29 18:31:27 -0300

    Function 11: done

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,8 +13,42 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let result = '';
+  let count = 0;
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let index1 in array) {
+      if (array[index1] < 0 || array[index1] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      for (let index2 in array) {
+        if (array[index1] === array[index2]) {
+          count += 1;
+        }
+      }
+      if (count >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      count = 0;
+    }    
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (index === 0) {
+      result += '(';
+      result += array[index];
+    } else if (index === 1) {
+      result += array[index];
+      result += ') ';
+    } else if (index === 6) {
+      result += array[index];
+      result += '-';
+    } else {
+      result += array[index];
+    }
+  }
+  return result;
 }
 
 // Desafio 12
@@ -33,4 +67,5 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
 
