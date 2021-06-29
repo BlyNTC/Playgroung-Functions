f9c204bf31afaad356956cd820a06b896c2fff3c
Pedro Figueiredo <pedrofigueiredo25@outlook.com>
2021-06-29 17:01:24 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,20 +1,19 @@
 // Desafio 1
 function compareTrue(param1, param2) {
-  if (param1 && param2 == true){
+  if (param1 && param2 == true) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
-compareTrue ()
+compareTrue()
 
 // Desafio 2
 function calcArea(base, height) {
-  area = ((base * height)/2);
+  area = ((base * height) / 2);
   return area;
 }
-calcArea (51, 1);
+calcArea(51, 1);
 
 // Desafio 3 - material consultado para resolução https://www.w3schools.com/jsref/jsref_split.asp
 function splitSentence(stringParam) {
@@ -25,14 +24,21 @@ function splitSentence(stringParam) {
 splitSentence('go Trybe');
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(paramArray) {
+  let primeiroIndice = paramArray [0]
+  let ultimoIndice = paramArray[paramArray.lenght -1]
 }
 
+concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontosVitoria = 3;
+  let pontosEmpate = 1;
+  pontosTotais = (wins * pontosVitoria) + (ties * pontosEmpate);
+  return pontosTotais; 
 }
+footballPoints()
 
 // Desafio 6
 function highestCount() {
@@ -53,6 +59,7 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
