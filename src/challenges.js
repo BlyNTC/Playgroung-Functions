0c3ead41f68a007c577fa624aa98919da464cae1
dandara-dias <dandaradias.contato@gmail.com>
2021-06-29 19:42:16 -0300

    Caça ao rato

--- src/challenges.js
+++ src/challenges.js
@@ -51,8 +51,17 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distancia1 = mouse - cat1;
+  let distancia2 = mouse - cat2;
+
+  if(Math.abs(distancia1) < Math.abs(distancia2)){
+    return "cat1";
+  } else if(Math.abs(distancia1) > Math.abs(distancia2)){
+    return "cat2";
+  } else{
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
