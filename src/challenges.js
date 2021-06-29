feb7fef3de8aae1088f68b6f90855aabfb48e914
rodPinheiro <rod_bma@hotmail.com>
2021-06-29 16:25:18 -0300

    Desafio 8 PASSED

--- src/challenges.js
+++ src/challenges.js
@@ -64,8 +64,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arr) {
+  result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+      result.push("fizzBuzz");
+    } else if (arr[i] % 3 === 0) {
+      result.push("fizz")
+    } else if (arr[i] % 5 === 0) {
+      result.push("buzz")
+    } else {
+      result.push("bug!")
+    }
+  }
+  return result;
 }
 
 // Desafio 9
