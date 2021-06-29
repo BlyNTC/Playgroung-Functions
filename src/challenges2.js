601d9481e5551cf9e7fae009250c5099f3ee2e22
darthyd <luaks.skt8@gmail.com>
2021-06-29 15:50:48 -0300

    challenge 10: techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  if (array.length === 0) return 'Vazio!';
+  const arrayObj = [];
+  array.sort().forEach((element) => {
+    arrayObj.push(
+      {
+        tech: element,
+        name,
+      },
+    );
+  });
+  return arrayObj;
 }
 
 // Desafio 11
