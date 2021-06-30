8ce54c29a19ee5980113c965db6f74044ce6f0d1
Flavio Pires <flavioasp77@gmail.com>
2021-06-30 13:18:08 -0300

    Enviando desafio 4 para avaliaçao

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,14 @@ function splitSentence(frase) {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  let resultado = "";
+  resultado += array[array.length -1];
+  resultado += ", ";
+  resultado += array[0];
+
+  return resultado;
 }
 
 // Desafio 5
