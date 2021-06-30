422dd9fb57e416715b737973a8519394d326a100
Michel <ferrazmichel@gmail.com>
2021-06-30 18:40:42 -0300

    Ajustes 4

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 // Desafio 10
 function techList(array, name) {
   let techlist = [];
@@ -9,44 +10,34 @@ function techList(array, name) {
     };
     techlist.push(object);
   }
-  if (techlist === '') {
+  // eslint-disable-next-line eqeqeq
+  if (techlist == '') {
     return 'Vazio!';
-  } 
+  }
   return techlist;
 }
 
 // Desafio 11
+// eslint-disable-next-line complexity
+// eslint-disable-next-line sonarjs/cognitive-complexity
 function generatePhoneNumber(array) {
-  var number = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+  let number = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
   let count = 0;
+  let msg = 'não é possível gerar um número de telefone com esses valores';
   for (let a = 0; a <= 9; a++) {
     for (let b = 0; b < array.length; b++) {
-      if (array[b] == a) {
-        count = count + 1;
-      }
-      if (count > 2) {
-        number = 'não é possível gerar um número de telefone com esses valores';  
-      }
+      if (array[b] == a) {count = count + 1;}
+      if (count > 2) { number = msg;}
     }
-    count = 0;
-
+  count = 0;
   }
-
   for (let i = 0; i < array.length; i++) {
-    if (array[i] < 0) {
-      number = 'não é possível gerar um número de telefone com esses valores';
-    } else if (array[i] > 9) {
-      number = 'não é possível gerar um número de telefone com esses valores';
-    }
+    if (array[i] < 0) { number = msg;} 
+    else if (array[i] > 9) { number = msg;}
   }
-
-  if (array.length != 11) {
-    number = 'Array com tamanho incorreto.'
-  }
-
+  if (array.length != 11) { number = 'Array com tamanho incorreto.'}
   return number;
 }
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   var result;
