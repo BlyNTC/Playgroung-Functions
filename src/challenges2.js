ceab74caee38a376e601a9445ab2c5bd078eb29c
Robert Nascimento <robert1a7x@gmail.com>
2021-06-30 19:12:23 -0300

    Finalizado o requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,8 +4,7 @@ function techList(array, name) {
   array.sort();
   for (let i of array) {
     objectArray.push({
-      tech: i,
-      name: name
+      tech: i, name: name
     });
   }
   if (objectArray.length === 0) {
@@ -16,8 +15,26 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let arrayMaxNumber = Math.max(...array);
+    let contador = 0;
+    if (array.length !== 11) {
+        return "Array com tamanho incorreto.";
+    } else {
+        for (let i = 0; i < array.length; i += 1) {
+            contador = 0;
+            for (let h = 0; h < array.length; h += 1) {
+                if (array[i] === array[h]) {
+                    contador += 1;
+                }
+                if (array[i] < 0 || arrayMaxNumber > 9 || contador >= 3) {
+                    return "não é possível gerar um número de telefone com esses valores";
+                }
+            }
+        }
+    }
+    let numero = "(" + array[0] + array[1] + ")" + " " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
+    return numero;
 }
 
 // Desafio 12
