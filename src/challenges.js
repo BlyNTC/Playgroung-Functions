444c5596dd3f3758429009e0a4ed7068406922de
Matheus Alves <malves224@gmail.com>
2021-06-29 21:28:49 -0300

    resolve requisito 6 do projeto e corrigi o requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,9 @@
 function compareTrue(valor1, valor2) {
   if (Boolean(valor1) === true && Boolean(valor2) === true) {
     return true;
-    } else {
-     return false;
-    }
+  } else {
+    return false;
+  }
 }
 // Desafio 2
 function calcArea(area, height) {
@@ -16,40 +16,55 @@ function splitSentence(string) {
   let outputArray = [];
   let palavra;
   let indexDoEspacoDeRetomada = 0;
-  for (let index = 0; index < string.length; index += 1){
-    if (string[index] === ' ' ){
-      for ( let index2 = indexDoEspacoDeRetomada; index2 <= index; index2 += 1){
-          palavra = palavra + string[index2];
-      } 
-      outputArray.push(palavra);
-      indexDoEspacoDeRetomada = index + 1
-  } else if (index == string.length-1 ){
-      for (let index2 = indexDoEspacoDeRetomada; index2 <= string.length-1; index2 += 1){
-          palavra = palavra + string[index2];
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index] === ' ') {
+      for (let index2 = indexDoEspacoDeRetomada; index2 < index; index2 += 1) {
+        palavra = palavra + string[index2];
       }
       outputArray.push(palavra);
+      indexDoEspacoDeRetomada = index + 1
+    } else if (index === string.length - 1) {
+      for (let index2 = indexDoEspacoDeRetomada; index2 <= string.length - 1; index2 += 1) {
+        palavra = palavra + string[index2];
+      }
+      outputArray.push(palavra);
+    }
+    palavra = '';
   }
-  palavra = '';
-  }
-  return outputArray; 
+  return outputArray;
 }
+console.log(splitSentence('vamo que vamo'))
 // Desafio 4
 function concatName(array) {
-  return array[array.length-1] + ', ' + array[0] 
+  return array[array.length - 1] + ', ' + array[0]
 }
-
 // Desafio 5
 function footballPoints(win, ties) {
   let pontosVitorias = win * 3;
   let pontosEmpates = ties * 1;
   return (pontosVitorias + pontosEmpates);
 }
-console.log(footballPoints(1, 2));
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maiorNumero = 0;
+  let output = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > array[index + 1]) {
+      if (maiorNumero < array[index])
+        maiorNumero = array[index]
+    } else {
+      if (array[array.length - 1] > maiorNumero) {
+        maiorNumero = array[array.length - 1]
+      }
+    }
+  }
+  for (let index in array) {
+    if (maiorNumero === array[index]) {
+      output += 1
+    }
+  }
+  return (output);
 }
-
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
@@ -64,6 +79,7 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
