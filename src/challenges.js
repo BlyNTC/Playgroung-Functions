673f857e0580ce056d28ede078e01d6906c1b38f
hector <hectoreddbenitez@gmail.com>
2021-06-29 21:29:36 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -11,21 +11,24 @@ function compareTrue(n1,n2) {
   }
 }
 
-console.log(compareTrue(true,true))
-
 
 // Desafio 2
 
 
 
-function calcArea() {
-  
+function calcArea(base,height) {
+  return (base * height)/2
 }
 
+
+
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+
+
+function splitSentence(string){
+  return string.split(' ')   
 }
+
 
 // Desafio 4
 function concatName() {
