5bd82cf92487b0e2daf5250942c722fa566285c4
Thiago de Oliveira <thiago.so@hotmail.com>
2021-06-30 01:32:31 -0300

    desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,8 @@
-// Desafio 1
+// Desafio 1 OK
 function compareTrue(valor1, valor2) {
   // seu código aqui
+  // na monitoria Ítalo explicou que todas essas linhas de código podem ser substituidas por
+  // "return valor1 && valor2" pq, quando os parâmetros são booleanos a comparação fica restrira a true e false
   if (valor1 === true && valor2 === true) {
     return true;
   } else {
@@ -8,28 +10,38 @@ function compareTrue(valor1, valor2) {
   }
 }
 
-// Desafio 2
+// Desafio 2 OK
 function calcArea(base, height) {
   // seu código aqui
   return (base * height) / 2;
 }
-console.log(calcArea(51, 1));
 
-
-// Desafio 3
-function splitSentence() {
+// Desafio 3 OK
+function splitSentence(string = []) {
   // seu código aqui
+  for (caractere of string) {
+    return string.split(' ');
+  }
 }
 
-// Desafio 4
-function concatName() {
+// Desafio 4 NÃO
+function concatName(array) {
   // seu código aqui
+  let primeiroItem = array;
+  let ultimoItem = array[array.lenght - 1];
+  let result = ultimoItem.concat(primeiroItem, ', ');
+  return result;
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let vitoria = 3;
+  let empate = 1;
+  return ((wins * vitoria) + (ties * empate));
 }
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
