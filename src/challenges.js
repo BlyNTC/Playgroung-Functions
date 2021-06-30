91e48c3abd293ecb56143ccbc5abec7b841e97da
Michel <ferrazmichel@gmail.com>
2021-06-30 18:59:12 -0300

    Ajustes 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,3 +1,11 @@
+/* eslint-disable sonarjs/cognitive-complexity */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable complexity */
+/* eslint-disable max-lines-per-function */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-mixed-operators */
 // Desafio 1
 function compareTrue(n1, n2) {
   if (n1 && n2) {
@@ -14,13 +22,13 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  let stringarray = string.split(" ");
+  let stringarray = string.split(' ');
   return stringarray;
 }
 
 // Desafio 4
 function concatName(array) {
-  let string = array[array.length-1] + ", " + array[0];
+  let string = `${array[array.length - 1]}, ${array[0]}`;
   return string;
 }
 
@@ -47,22 +55,19 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let cat1d = cat1 - mouse;
   let cat2d = cat2 - mouse;
-  let result = "";
-
+  let result = '';
   if (cat1d < 0) {
-    cat1d = cat1d * -1;
+    cat1d *= -1;
   }
-
   if (cat2d < 0) {
-    cat2d = cat2d * -1;
+    cat2d *= -1;
   }
-
   if (cat1d > cat2d) {
-    result = "cat2"
+    result = 'cat2';
   } else if (cat1d < cat2d) {
-    result = "cat1"
+    result = 'cat1';
   } else {
-    result = "os gatos trombam e o rato foge";
+    result = 'os gatos trombam e o rato foge';
   }
   return result;
 }
@@ -73,27 +78,26 @@ function fizzBuzz(array) {
   let final = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i] % 3 == 0) {
-      var a = "fizz";
+      let a = 'fizz';
     } else {
-      var a = "null";
+      let a = 'null';
     }
     if (array[i] % 5 == 0) {
-      var b = "buzz";
+      let b = 'buzz';
     } else {
-      b = "null";
-    }
-    var c = a + b;
+      b = 'null';}
+    let c = a + b;
     result.push(c);
   }
   for (let i = 0; i < result.length; i++) {
-    if (result[i] == "nullnull") {
-      final.push("bug!");
-    } else if (result[i] == "fizzbuzz") {
-      final.push("fizzBuzz");
-    } else if (result[i] == "fizznull") {
-      final.push("fizz");
-    } else if (result[i] == "nullbuzz") {
-      final.push("buzz");
+    if (result[i] == 'nullnull') {
+      final.push('bug!');
+    } else if (result[i] == 'fizzbuzz') {
+      final.push('fizzBuzz');
+    } else if (result[i] == 'fizznull') {
+      final.push('fizz');
+    } else if (result[i] == 'nullbuzz') {
+      final.push('buzz');
     }
   }
   return final;
@@ -101,45 +105,45 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(string) {
-  let array = string.split("");
+  let array = string.split('');
   let final = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] == "a") {
-      final.push("1");
-    } else if (array[i] == "e") {
-      final.push("2");
-    } else if (array[i] == "i") {
-      final.push("3");
-    } else if (array[i] == "o") {
-      final.push("4");
-    } else if (array[i] == "u") {
-      final.push("5");
+    if (array[i] == 'a') {
+      final.push('1');
+    } else if (array[i] == 'e') {
+      final.push('2');
+    } else if (array[i] == 'i') {
+      final.push('3');
+    } else if (array[i] == 'o') {
+      final.push('4');
+    } else if (array[i] == 'u') {
+      final.push('5');
     } else {
       final.push(array[i]);
     }
   }
-  var result = final.join("");
+  let result = final.join('');
   return result;
 }
 function decode(string) {
-  let array = string.split("");
+  let array = string.split('');
   let final = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] == "1") {
-      final.push("a");
-    } else if (array[i] == "2") {
-      final.push("e");
-    } else if (array[i] == "3") {
-      final.push("i");
-    } else if (array[i] == "4") {
-      final.push("o");
-    } else if (array[i] == "5") {
-      final.push("u");
+    if (array[i] == '1') {
+      final.push('a');
+    } else if (array[i] == '2') {
+      final.push('e');
+    } else if (array[i] == '3') {
+      final.push('i');
+    } else if (array[i] == '4') {
+      final.push('o');
+    } else if (array[i] == '5') {
+      final.push('u');
     } else {
       final.push(array[i]);
     }
   }
-  var result = final.join("");
+  let result = final.join('');
   return result;
 }
 
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,20 @@
+/* eslint-disable sonarjs/cognitive-complexity */
+/* eslint-disable sonarjs/no-duplicated-branches */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable radix */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
 /* eslint-disable complexity */
 // Desafio 10
 function techList(array, name) {
   let techlist = [];
   array.sort();
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < array.length; i++) {
     let object = {
       tech: array[i],
+      // eslint-disable-next-line object-shorthand
       name: name,
     };
     techlist.push(object);
@@ -21,13 +30,14 @@ function techList(array, name) {
 // eslint-disable-next-line complexity
 // eslint-disable-next-line sonarjs/cognitive-complexity
 function generatePhoneNumber(array) {
-  let number = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+  let number = `(${array[0]}${array[1]}) ${array[2]}${array[3] 
+  }${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
   let count = 0;
   let msg = 'não é possível gerar um número de telefone com esses valores';
   for (let a = 0; a <= 9; a++) {
     for (let b = 0; b < array.length; b++) {
-      if (array[b] == a) {count = count + 1;}
-      if (count > 2) { number = msg;}
+      if (array[b] == a) { count += 1; }
+      if (count > 2) { number = msg; }
     }
   count = 0;
   }
@@ -40,7 +50,7 @@ function generatePhoneNumber(array) {
 }
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  var result;
+  let result;
   if (lineA > lineB + lineC) {
     result = false;
   } else if (lineB > lineA + lineC) {
@@ -48,32 +58,31 @@ function triangleCheck(lineA, lineB, lineC) {
   } else if (lineC > lineB + lineA) {
     result = false;
   }
-
   if (lineA < Math.abs(lineB - lineC)) {
     result = false;
   } else if (lineB < Math.abs(lineA - lineC)) {
     result = false;
   } else if (lineC < Math.abs(lineB - lineA)) {
     result = false;
-  } else {
-    result = true;
-  }
+    } else {
+      result = true;
+      }
   return result;
 }
 
 // Desafio 13
 function hydrate(string) {
-  var regex = /\d+/g;
-  var matches = string.match(regex);
-  var qtd = 0;
+  let regex = /\d+/g;
+  let matches = string.match(regex);
+  let qtd = 0;
   for (let i = 0; i < matches.length; i += 1) {
-    var int = parseInt(matches[i]);
+    let int = parseInt(matches[i]);
     qtd += int;
   }
   if (qtd > 1) {
-    var text = qtd + ' copos de água'
+    let text = `${qtd} copos de água`;
   } else {
-  var text = '1 copo de água'
+  let text = '1 copo de água';
   }
   return text;
 }
