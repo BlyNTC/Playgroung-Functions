8687fb569aaf67f78fea4f575df08824742d615d
BlyNTC <isaacase@hotmail.com>
2021-06-29 21:06:07 -0300

    formata com beatify

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(requisito1, requisito2) {
-  let trueFalse = requisito1&&requisito2
+  let trueFalse = requisito1 && requisito2
   return (trueFalse)
   // seu código aqui
 }
@@ -22,14 +22,14 @@ function splitSentence(frase) {
 
 // Desafio 4
 function concatName(listNames) {
-  return (listNames[listNames.length -1]+', '+listNames[0])
+  return (listNames[listNames.length - 1] + ', ' + listNames[0])
 
   // seu código aqui
 }
 
 // Desafio 5
 function footballPoints(win, ties) {
-  return ((win * 3)+ties)
+  return ((win * 3) + ties)
   // seu código aqui
 }
 
@@ -38,25 +38,25 @@ function highestCount(numeros) {
   let numeroMaior = Math.max(...numeros);
   let count = 0;
   for (let i of numeros) {
-    if(numeroMaior === i){
+    if (numeroMaior === i) {
       count += 1
     }
-    }return (count)
   }
-  // seu código aqui
+  return (count)
+}
+// seu código aqui
 
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let cat11 = cat1-mouse
-  let cat22 = cat2-mouse
-  if(Math.abs(cat11) < Math.abs(cat22)){
+  let cat11 = cat1 - mouse
+  let cat22 = cat2 - mouse
+  if (Math.abs(cat11) < Math.abs(cat22)) {
     return ('cat1')
   }
-  if(Math.abs(cat11) > Math.abs(cat22)){
+  if (Math.abs(cat11) > Math.abs(cat22)) {
     return ('cat2')
-  }
-  else {
+  } else {
     return ("os gatos trombam e o rato foge")
   }
 }
@@ -64,19 +64,18 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numeros) {
   let numero = []
-    for (const i of numeros) {
-      if (i % 3 == 0 && i % 5 == 0) {
-        numero.push("fizzBuzz")
-      }
-     else if (i % 3 == 0) {
-        numero.push('fizz')
-      }
-      else if (i % 5 == 0) {
-        numero.push('buzz')
-      }
-      else {numero.push('bug!')}
+  for (const i of numeros) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      numero.push("fizzBuzz")
+    } else if (i % 3 == 0) {
+      numero.push('fizz')
+    } else if (i % 5 == 0) {
+      numero.push('buzz')
+    } else {
+      numero.push('bug!')
     }
-    return(numero)
+  }
+  return (numero)
   // seu código aqui
 }
 
@@ -104,36 +103,39 @@ function encode(string) {
         stringencode += i
         break;
     }
-    
-  }return(stringencode)
+
+  }
+  return (stringencode)
   // seu código aqui
 }
+
 function decode(string) {
   let stringdecode = ''
   for (let i of string) {
     switch (i) {
       case '1':
-        stringdecode +='a'
+        stringdecode += 'a'
         break;
       case '2':
-        stringdecode +='e'
+        stringdecode += 'e'
         break;
       case '3':
-        stringdecode +='i'
+        stringdecode += 'i'
         break;
       case '4':
-        stringdecode +='o'
+        stringdecode += 'o'
         break;
       case '5':
-        stringdecode +='u'
+        stringdecode += 'u'
         break;
       default:
         stringdecode += i
         break;
     }
-  }return(stringdecode)
+  }
+  return (stringdecode)
 }
-  // seu código aqui
+// seu código aqui
 
 
 module.exports = {
