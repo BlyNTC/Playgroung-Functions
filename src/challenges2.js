c40d3a49d7036ae6b91e96b70301217afa8ce08c
Michel <ferrazmichel@gmail.com>
2021-06-30 18:31:13 -0300

    Ajustes 3

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,31 +5,27 @@ function techList(array, name) {
   for (let i = 0; i < array.length; i++) {
     let object = {
       tech: array[i],
-      name: name
-    }
+      name: name,
+    };
     techlist.push(object);
   }
-  if (techlist == "") {
-    return "Vazio!";
-  } else {
+  if (techlist === '') {
+    return 'Vazio!';
+  } 
   return techlist;
-  }
 }
 
 // Desafio 11
 function generatePhoneNumber(array) {
- 
-  var number = "(" + array[0] + array[1] + ") " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
-  
+  var number = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
   let count = 0;
-
   for (let a = 0; a <= 9; a++) {
     for (let b = 0; b < array.length; b++) {
       if (array[b] == a) {
         count = count + 1;
       }
       if (count > 2) {
-        number = "não é possível gerar um número de telefone com esses valores";  
+        number = 'não é possível gerar um número de telefone com esses valores';  
       }
     }
     count = 0;
@@ -38,14 +34,14 @@ function generatePhoneNumber(array) {
 
   for (let i = 0; i < array.length; i++) {
     if (array[i] < 0) {
-      number = "não é possível gerar um número de telefone com esses valores";
+      number = 'não é possível gerar um número de telefone com esses valores';
     } else if (array[i] > 9) {
-      number = "não é possível gerar um número de telefone com esses valores";
+      number = 'não é possível gerar um número de telefone com esses valores';
     }
   }
 
   if (array.length != 11) {
-    number = "Array com tamanho incorreto."
+    number = 'Array com tamanho incorreto.'
   }
 
   return number;
@@ -84,9 +80,9 @@ function hydrate(string) {
     qtd += int;
   }
   if (qtd > 1) {
-    var text = qtd + " copos de água"
+    var text = qtd + ' copos de água'
   } else {
-  var text = "1 copo de água"
+  var text = '1 copo de água'
   }
   return text;
 }
