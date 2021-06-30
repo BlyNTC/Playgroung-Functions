a21eec8be1cb04efd520e934f8aee99947b08f01
Pablo <pabloassuncao2@gmail.com>
2021-06-29 22:54:31 -0300

    C11 - Adiciona o decimo primeiro requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,22 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phone) {
   // seu código aqui
+  if (phone.length !== 11){
+    return "Array com tamanho incorreto."
+  }else{
+    let numRepeat = phone.reduce(function(reps, nums){
+      reps[nums] = (reps[nums] ? reps[nums]+=1 : 1);
+      return reps;
+    }, {})
+    if (phone.some((x) => x < 0 || x > 9 || numRepeat[x] >= 3)){ return "não é possível gerar um número de telefone com esses valores";
+  }else{
+      let result = "(" + phone[0] + phone[1] + ") " + phone[2] + phone [3] + phone[4] + phone[5] + phone[6] + "-" + phone[7] + phone[8] + phone[9] + phone [10]
+
+      return result
+    }
+  }
 }
 
 // Desafio 12
