59d20e441aeae4b26ce867c9ddf813b60172db0d
Robert Nascimento <robert1a7x@gmail.com>
2021-06-30 19:08:14 -0300

    Finalizado o requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,18 @@
 // Desafio 10
-function techList() {
-
+function techList(array, name) {
+  let objectArray = [];
+  array.sort();
+  for (let i of array) {
+    objectArray.push({
+      tech: i,
+      name: name
+    });
+  }
+  if (objectArray.length === 0) {
+    return 'Vazio!';
+  } else {
+      return objectArray;
+  }
 }
 
 // Desafio 11
