fd1687077452ee3f65c9075e33c1fed8e4f5c0af
Écio Ferraz <ecioferraz@hotmail.com>
2021-06-30 11:43:11 -0300

    requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,20 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  // seu código aqui
+  //   let phoneNumber;
+//   console.log(phoneNumber)
+//   let response;
+//   for (let index in numbers) {
+//     if (numbers.length !== 11) {
+//       response = 'Array com tamanho incorreto';
+//     } else if (numbers[index] < 0 || numbers[index] > 9) {
+//       response = 'não é possível gerar um número de telefone com esses valores';
+//     } else if {
+//       phoneNumber = Math.ceil(Math.random() * numbers[index])}
+//     return response;
+//   }
 }
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -33,8 +45,18 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let alcoholCups = string.replace(/\D/g, '');
+  let alcoholArray = alcoholCups.split('').map(Number);
+  let sum = 0;
+  for (index = 0; index < alcoholArray.length; index += 1) {
+    sum += alcoholArray[index];
+  }
+  if (sum === 1) {
+    return sum + ' copo de água';
+  } else {
+    return sum + ' copos de água';
+  }
 }
 
 module.exports = {
