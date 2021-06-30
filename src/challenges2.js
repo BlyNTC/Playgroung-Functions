a05cdafb0af29dd5e2020bc757f38c8fa6a3fa62
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2021-06-29 23:08:16 -0300

    Faz nLint do phoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,23 @@
+// Funções auxiliares do Desafio 11
+function overThreeRepeats(inputArray) {
+  const counts = {};
+  let checker = false;
+  inputArray.forEach(function (x) {
+    counts[x] = (counts[x] || 0) + 1;
+    if (counts[x] > 2) { checker = true; }
+  });
+  return checker;
+}
+
+function formatPhoneNumber(inputArray) {
+  inputArray.splice(0, 0, '(');
+  inputArray.splice(3, 0, ')');
+  inputArray.splice(9, 0, '-');
+  inputArray.splice(4, 0, ' ');
+  return inputArray;
+}
+// Fim das funções auxiliares
+
 // Desafio 10
 function techList(arrayTech, nomeString) {
   if (arrayTech.length === 0) {
@@ -16,40 +36,14 @@ function techList(arrayTech, nomeString) {
   return newArray;
 }
 
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'Javascript'], 'Lucas'));
-
 // Desafio 11
 function generatePhoneNumber(inputArray) {
-  let myPhoneNumber = '';
   const error = 'não é possível gerar um número de telefone com esses valores';
   let sorted = inputArray.slice().sort((a, b) => a - b);
-  if (inputArray.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  if (sorted[0] < 0 || sorted[sorted.length - 1] > 9) {
-    return error;
-  }
-  // Se eu soubesse separar um array em arrays de valores repetidos, eu poderia usar o .length para checar se nenhum numero se
-  // repete 3 ou mais vezes e diminuir a complexity e cognitive complexity do meu codigo. - Mas eu não sei! Achei um codigo que
-  // faz isso na internet e não entendi.
-  for (let index in inputArray) {
-    if (inputArray.hasOwnProperty.call(inputArray, index)) {
-      let counter = 0;
-      inputArray.forEach(function (value) {
-        if (value === inputArray[index]) {
-          counter += 1;
-        }
-      });
-      if (counter > 2) {
-        return error;
-      }
-      myPhoneNumber += inputArray[index];
-    }
-  }
-  return '('.concat(myPhoneNumber.substr(0, 2)).concat(')')
-    .concat(' ').concat(myPhoneNumber.substr(2, 5))
-    .concat('-')
-    .concat(myPhoneNumber.substr(7));
+  if (inputArray.length !== 11) { return 'Array com tamanho incorreto.'; }
+  if (sorted[0] < 0 || sorted[sorted.length - 1] > 9) { return error; }
+  if (overThreeRepeats(inputArray) === true) { return error; }
+  return formatPhoneNumber(inputArray).join('');
 }
 
 // Desafio 12
@@ -64,8 +58,6 @@ function triangleCheck(lineA, lineB, lineC) {
   }
   return false;
 }
-
-triangleCheck(16, 9, 2);
 
 // Desafio 13
 function hydrate(string) {
@@ -84,3 +76,5 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
