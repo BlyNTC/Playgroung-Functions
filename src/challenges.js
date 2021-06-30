6e3462b3976ab4aec79b5d3eb9704f4ffd387d48
Veruska de Oliveira <veruska.ideasdigital@gmail.com>
2021-06-30 10:47:46 -0300

    Challenges I e II

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,137 @@
-// Desafio 1
-function compareTrue() {
-  // seu código aqui
+// Desafio 1 - Crie uma função usando o operador &&
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true){
+    return true
+  } else {
+    return false
+  }
 }
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
+// Desafio 2 - Crie uma função que calcule a área de um triângulo
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+// Desafio 3 - Crie uma função que divida a frase
+function splitSentence(string) {
+  return string.split(' ');
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+// Desafio 4 - Crie uma função que use concatenação de strings
+function concatName(names) {
+  return names[names.length - 1] + ', ' + names[0];
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+// Desafio 5 - Crie uma função que calcule a quantidade de pontos no futebol
+function footballPoints(wins, ties) {
+  return wins * 3 + ties ;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Desafio 6 - Crie uma função que calcule a repetição do maior número
+function highestCount(numbers) {
+
+  let numMaior = 0;
+  let indexMaior = 0;
+  let contador = 0;
+
+  for (let index in numbers) {
+
+    if (numbers[indexMaior] < numbers[index]) {
+
+      indexMaior = index;
+    }
+
+    numMaior = numbers[indexMaior]
+  }
+
+  for (let num of numbers) {
+
+    if (numMaior === num) {
+
+      contador += 1;
+    }
+  }
+  return contador;
+}
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
+
+// Desafio 7 - Crie uma função de Caça ao Rato
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)) {
+    return 'os gatos trombam e o rato foge';
+  }
+  return mouse + cat1 > mouse + cat2 ? 'cat2' : 'cat1';
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Desafio 8 - Crie uma função FizzBuzz
+function fizzBuzz(numbers) {
+  let answer = [];
+  for (const iterator of numbers) {
+    if (iterator % 3 === 0 && iterator % 5 === 0) {
+      answer.push('fizzBuzz');
+    } else if (iterator % 3 === 0) {
+      answer.push('fizz');
+    } else if (iterator % 5 === 0) {
+      answer.push('buzz');
+    } else {
+      answer.push('bug!');
+    }
+  }
+  return answer;
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// Desafio 9 - Crie uma função que Codifique e Decodifique
+function encode(phrase) {
+  let newPhrase = '';
+  for (let key in phrase) {
+    switch (phrase[key]) {
+    case 'a':
+      newPhrase += 1;
+      break;
+    case 'e':
+      newPhrase += 2;
+      break;
+    case 'i':
+      newPhrase += 3;
+      break;
+    case 'o':
+      newPhrase += 4;
+      break;
+    case 'u':
+      newPhrase += 5
+      break;
+    default:
+      newPhrase += phrase[key]
+    }
+  }
+  return newPhrase;
 }
-
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+function decode(phrase) {
+  let newPhrase = '';
+  for (let key in phrase) {
+    switch (phrase[key]) {
+    case '1':
+      newPhrase += 'a';
+      break;
+    case '2':
+      newPhrase += 'e';
+      break;
+    case '3':
+      newPhrase += 'i';
+      break;
+    case '4':
+      newPhrase += 'o';
+      break;
+    case '5':
+      newPhrase += 'u';
+      break;
+    default:
+      newPhrase += phrase[key]
+    }
+  }
+  return newPhrase;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,94 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+// Desafio 10 - Crie uma função de Lista de Tecnologias
+function techList(techs, name) {
+
+  if (techs.length === 0 && name === 'Lucas') {
+
+    return "Vazio!";
+
+  } else {
+
+    let imput = [];
+
+    for (let key of techs) {
+
+      let outline = {
+        tech: '',
+        name: ''
+      }
+
+      outline.tech = key;
+      outline.name = name;
+
+      imput.push(outline)
+    }
+// fonte para função sort = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    return imput.sort(function(a, b){
+      if (a.tech > b.tech) {
+        return 1
+      }
+      if (a.tech < b.tech) {
+        return -1;
+      }
+      return 0;
+    });
+
+  }
+
+}
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
+
+// Desafio 11 - Crie uma função de Número de Telefone
+function generatePhoneNumber(numberPhone) {
+  let list = '';
+  if (numberPhone.length !== 11) {
+   return "Array com tamanho incorreto."
+  }
+  for (let num in numberPhone) {
+    let cont = 0;
+    for (let key in numberPhone) {
+      if (numberPhone[key] === numberPhone[num]) {
+        cont += 1;
+      }
+    }
+    if (numberPhone[num] < 0 || numberPhone[num] > 9 || cont >= 3 ) {
+      return "não é possível gerar um número de telefone com esses valores";
+    } else if (num == 0) {
+      list += '(';
+    } else if (num == 2) {
+      list += ') '
+    } else if (num == 7) {
+      list += '-'
+    }
+    list += numberPhone[num];
+  }
+  return list;
 }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// Desafio 12 - Crie uma função de Condição de existência de um triângulo
+function triangleCheck(lineA, lineB, lineC) {
+  if (Math.abs((lineB - lineC || lineB - lineC)) < lineA && lineA < (lineB + lineC)){
+    return true;
+  } else if (Math.abs((lineA - lineC) || (lineA - lineC)) < lineB && lineB < (lineA + lineC)){
+    return true;
+  } else if (Math.abs((lineA - lineB) || (lineA - lineB)) < lineC && lineC < (lineA + lineB)){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
+// Desafio 13 - Crie uma função de boas vindas ao Bar da Trybe!
+function hydrate(pedido) {
+  let list = 0;
+  for (let key of pedido) {
+   if (!isNaN(key) && key !== ' ')  {
+     list += parseFloat(key);
+   }
+ }
+ if (list === 1) {
+   return list + ' copo de água';
+ }
+ return list + ' copos de água';
 }
 
 module.exports = {
