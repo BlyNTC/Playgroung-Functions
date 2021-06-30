6144b836c7a831b4a5749543cc13d7e6b7226140
WalmirLucena <walmirlucena3@gmail.com>
2021-06-30 13:10:24 -0400

    Atualiza questao 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,38 +23,36 @@ function generatePhoneNumber(array) {
   // seu código aqui
   let numero = '(';
   let cont = 0;
-  if(array.length!=11){
+  if (array.length != 11) {
     return 'Array com tamanho incorreto.';
   }
- 
-  
-  for(let index=0; index<11; index+=1){
-    if(array[index]<0 || array[index]>9){
+
+
+  for (let index = 0; index < 11; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
       return 'não é possível gerar um número de telefone com esses valores';
-    } 
-    cont=0;
-    for(let index2=0; index2<11; index2 +=1){
-      if(array[index]===array[index2]){
-        cont+=1
+    }
+    cont = 0;
+    for (let index2 = 0; index2 < 11; index2 += 1) {
+      if (array[index] === array[index2]) {
+        cont += 1
       }
     }
-    if(cont >= 3){
+    if (cont >= 3) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
-     numero += array[index]  
-     if(index === 1){
-    numero += ') ';
-    } else if (index === 6){
+    numero += array[index]
+    if (index === 1) {
+      numero += ') ';
+    } else if (index === 6) {
       numero += '-';
-    } 
+    }
   }
-  
+
   return numero;
 
-  }
-  let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-  console.log(generatePhoneNumber(num))
-  
+}
+
 
 // Desafio 12
 function triangleCheck() {
