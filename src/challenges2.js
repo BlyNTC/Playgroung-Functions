c3513cf0f28ffff6c9f31fd6403e9393419f2302
Écio Ferraz <ecioferraz@hotmail.com>
2021-06-30 10:06:49 -0300

    requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let arrayTech = [];
+  let arrayTechSort = tech.sort();
+  for (index = 0; index < tech.length; index += 1) {
+    let object = {
+      tech: arrayTechSort[index],
+      name: name
+    }
+    arrayTech.push(object);
+  }
+  if (arrayTech.length === 0) {
+    return 'Vazio!'
+  } else {
+    return arrayTech;
+  }
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
