ccce958606b91db0870c91b1517d06211214cbcb
Filipe <andradefilipe2002@gmail.com>
2021-06-30 18:27:31 -0300

    Adiciona Função arrayTech

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  //Fonte de pesquisa StackOverFlow e Slack
+  let arrayTech = [];
+  array.sort();
+
+  if (array.length === 0) {
+    return 'Vazio!'
+  }
+
+  for (let tech of array) {
+    arrayTech.push({
+      tech,
+      name
+    });
+  }
+  return arrayTech
 }
 
 // Desafio 11
