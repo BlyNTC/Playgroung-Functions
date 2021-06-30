f2dc8b135c7742c77d23f88695e512d4714f7c03
Michel <ferrazmichel@gmail.com>
2021-06-29 21:47:17 -0300

    Bonus 1

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,25 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  var techlist = [];
+  array.sort();
+  for (let i = 0; i < array.length; i++) {
+    let object = {
+      tech: array[i],
+      name: name
+    }
+    techlist.push(object);
+  }
+  if (techlist == "") {
+    return "Vazio!";
+  } else {
+  return techlist;
+  }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  var number = "(" + array[0] + array[1] + ") " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
+  return number;
 }
 
 // Desafio 12
