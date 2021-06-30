48e5267c7746266e24d203d7c7da35d9e41e4811
Pedro Silva Buente <pedrobuente@gmail.com>
2021-06-30 13:07:56 -0300

    ultima tentativa do exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,33 +33,37 @@ function generatePhoneNumber(array) {
                 n += 1;
             }
         }
-            if (n === array.length) {
-                mensagem = '(' + array[0] + array[1] + ")" + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+              if (n === array.length) {
+                  mensagem = '(' + array[0] + array[1] + ")" + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+                  for (let numeros of array) {
+                    for (let num of array) {
+                        if (numeros === num) {
+                            repete += 1
+                        }
+        
+                    }
+                    if (repete > 3) {
+                        mensagem = "não é possível gerar um número de telefone com esses valores";
+                        break;
+                    }
+                    repete = 0;
+        
+                }
+                if (array.length != 11) {   
+    
+                  mensagem = "Array com tamanho incorreto.";
+              }
             } else {
                 mensagem = "não é possível gerar um número de telefone com esses valores";
             }
 
-        for (let numeros of array) {
-            for (let num of array) {
-                if (numeros === num) {
-                    repete += 1
-                }
+        
 
-            }
-            if (repete > 3) {
-                mensagem = "não é possível gerar um número de telefone com esses valores";
-                break;
-            }
-            repete = 0;
-
-        }
-
-      if (array.length != 11) {   
-    
-        mensagem = "Array com tamanho incorreto.";
-    }
+      
     return mensagem;
 }
+console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1,11]))
+
 
 
 // Desafio 12
