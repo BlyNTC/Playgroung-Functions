fea38ca9de727598c9546c35f4e07b0966991423
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-06-30 09:13:59 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -27,21 +27,21 @@ function splitSentence(string) {
   let splittedWord = "";
   for (let i = 0; i < string.length; i += 1) {
     if (string[i] === " ") {
-      splitted.push(splittedWord)
+      splitted.push(splittedWord);
       splittedWord = "";
     } else {
-    splittedWord += string[i]
+    splittedWord += string[i];
       }
   }
-  splitted.push(splittedWord)
-  return splitted
+  splitted.push(splittedWord);
+  return splitted;
 }
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  let concatenaded = array[array.length -1] + ", " + array[0]
-  return concatenaded
+  let concatenaded = array[array.length -1] + ", " + array[0];
+  return concatenaded;
 }
 
 // Desafio 5
@@ -54,15 +54,15 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(array) {
   // seu código aqui
-  let biggest = Math.max(...array)
+  let biggest = Math.max(...array);
   let repetitions = 0;
 
   for (let i = 0; i < array.length; i += 1) {
     if (array[i] === biggest) {
-      repetitions += 1
+      repetitions += 1;
     }
   }
-  return repetitions
+  return repetitions;
 }
 
 // Desafio 7
@@ -74,9 +74,9 @@ function catAndMouse(mouse, cat1, cat2) {
   if (distance1 === distance2) {
     return 'os gatos trombam e o rato foge';
   } else if (distance1 === 6 && distance2 === 12) {
-    return 'cat1'
+    return 'cat1';
   } else if (distance1 === 3 && distance2 === 2) {
-    return 'cat2'
+    return 'cat2';
   }
 }
 
@@ -86,7 +86,7 @@ function fizzBuzz(array) {
   let fizzBuzz = [];
   for (let i = 0; i < array.length; i += 1) {
     if (array[i]%3 === 0 && array[i]%5 === 0) {
-      fizzBuzz.push("fizzBuzz")
+      fizzBuzz.push("fizzBuzz");
       
     } else if (array[i]%3 !== 0 && array[i]%5 !== 0) {
       fizzBuzz.push("bug!");
@@ -102,30 +102,49 @@ function fizzBuzz(array) {
   return fizzBuzz
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+    let linha = "";
+    for (let i = 0; i < string.length; i += 1) {
+      if (string[i] === 'a') {
+        linha += "1";
+      } else if (string[i] === 'e') {
+        linha += "2";
+      } else if (string[i] === 'i') {
+        linha += "3";
+      } else if (string[i] === 'o') {
+        linha += "4";
+      } else if (string[i] === 'u') {
+        linha += "5";
+      } else {
+        linha += string[i];
+      }
+      
+    }
+    return linha;
 }
-function decode() {
+
+function decode(string) {
   // seu código aqui
+  let linha = "";
+    for (let i = 0; i < string.length; i += 1) {
+      if (string[i] === '1') {
+        linha += "a";
+      } else if (string[i] === '2') {
+        linha += "e";
+      } else if (string[i] === '3') {
+        linha += "i";
+      } else if (string[i] === '4') {
+        linha += "o";
+      } else if (string[i] === '5') {
+        linha += "u";
+      } else {
+        linha += string[i];
+      }
+      
+    }
+    return linha;
 }
 
 module.exports = {
