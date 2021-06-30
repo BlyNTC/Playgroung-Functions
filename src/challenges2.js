08fab58cec6756ed53baefb83212645902cf0f60
Heitor SF <heitor.sf5@gmail.com>
2021-06-29 21:54:42 -0300

    Implemented the paramaters to check if the phone number from Function 11 is valid or not. Willdo the other half later, or another day. Im tired.

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,14 +18,43 @@ function techList(learning, name) {
   }
 
   return listalinda;
-
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numbah) {
   // seu código aqui
+  let phonenumber;
+  let cachenumber;
+  let checkrepeat = 0;
+  
+  if (numbah.length != 11){
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  //checks if it's less than 0 and greater than 9
+  for (let key of numbah){
+    if (key < 0 || key > 9){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+
+    //checks if it repeats 3 times
+    for (let repeat of numbah){
+      if (key == repeat){
+        checkrepeat++;
+      }
+      else if (checkrepeat >= 3){
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    checkrepeat = 0;
+  }
+
+  //NOW it's gonna make the phone number jesus FUCK
+  
 
 }
+
+
 
 // Desafio 12
 function triangleCheck() {
