13a6c5d69afa02c97249014987e622637e8449b0
Pedro Silva Buente <pedrobuente@gmail.com>
2021-06-30 12:15:47 -0300

    exercicio bonus 11 numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,8 +23,41 @@ function techList(array,name) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let repete = 0;
+    let n = 0;
+    if (array.length === 11) {
+        for (let nume of array) {
+            if (nume <= 9 && nume >= 0) {
+                n += 1;
+            }
+        }
+            if (n === array.length) {
+                mensagem = '(' + array[0] + array[1] + ")" + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+            } else {
+                mensagem = "não é possível gerar um número de telefone com esses valores";
+            }
+
+        for (let numeros of array) {
+            for (let num of array) {
+                if (numeros === num) {
+                    repete += 1
+                }
+
+            }
+            if (repete > 3) {
+                mensagem = "não é possível gerar um número de telefone com esses valores";
+                break;
+            }
+            repete = 0;
+
+        }
+
+        
+    } else {
+        mensagem = 'Array com tamanho incorreto.';
+    }
+    return mensagem;
 }
 
 // Desafio 12
