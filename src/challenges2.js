e07613a0cedd291f82c917c72ff9493edf90e33a
Caio César Amorim <caiocavalcante063@gmail.com>
2021-06-30 11:55:06 -0300

    resolve desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
+function techList(arrTech, name) {
   // seu código aqui
+  let arrObj = [];
+  if (arrTech.length === 0) {
+    return 'Vazio!'
+  } else {
+      arrTech = arrTech.sort();
+      for (techs of arrTech) {
+        let bloco = {
+          tech: techs,
+          name: name
+        }
+        arrObj.push(bloco);
+      }
+      return arrObj;
+  }
 }
 
 // Desafio 11
