ad6011b5f37adc999e10afaf855fd4098c6cbe7b
p-lacerda <xcloudgameemail@gmail.com>
2021-06-30 18:27:31 -0300

    Resolve ultimas questoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, name) {
+  // FONTE DE PESQUISA STACKOVERFLOW E SLACK
+  let array = [];
+  arrayTech.sort();
+
+  if (arrayTech.length === 0) {
+    return 'Vazio!'
+  }
+
+  for (let tech of arrayTech) {
+    array.push({
+      tech,
+      name
+    });
+  }
+  return array
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
+
 
 // Desafio 11
 function generatePhoneNumber() {
