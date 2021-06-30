899fd90d36465dc048fccf2e6072647bf0a0ee06
Carla <carladmaximo@gmail.com>
2021-06-30 09:57:13 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -33,20 +33,32 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
+let contador = 0;
 function highestCount(numeros) {
   let maior = 0;
-  let contador = 0;
-  for (let index = 0; index = numeros.length; index += 1){
-    if (numeros[index] > maior) {
-      contador = contador + 1;
-    }
+  for(numero of numeros) {
+    //for (let index = 0; index === numeros.length; index += 1){
+      if (numero > maior) {
+        contador = contador + 1;
+      }
+    //}
   }
   return contador
 }
-
+console.log(contador)
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let d1 = Math.abs(mouse - cat1)
+  let d2 = Math.abs(mouse - cat2)
+  if (d1 < d1) {
+    return "cat1"
+  } else 
+  if (d2 < d1) {
+    return "cat2"
+  } else
+  if (d1 === d2) {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
@@ -65,16 +77,31 @@ function fizzBuzz(numeros) {
       palavras.push('bug!')
     }
         
-     }
+  }
   return palavras
   }  
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+/* Recebi ajuda da Anna Hamann, que me mandou o link https://www.w3schools.com/jsref/jsref_replace.asp
+- Eu Estava com dificuldade de usar o replace. No código que eu tinha feito, com "if" minha variável nao guardava todas as modificações, apenas a última. 
+- Tentei utilizar o switch case, com a sugestão do Thiago Oshiro, mas tive dificuldades em várias estruturas do tipo "for" e "if"
+- Com o uso do str.replace(/caracter-a-ser-mudado/g, 'caracter-que-quero-inserir') consigo armazenar todas as mudanças feitas em uma mesma variável.*/
+function encode(sentenca) {
+  sentenca = sentenca.replace (/a/g, '1'); 
+  sentenca = sentenca.replace (/e/g, '2');
+  sentenca = sentenca.replace (/i/g, '3');
+  sentenca = sentenca.replace (/o/g, '4');
+  sentenca = sentenca.replace (/u/g, '5');
+  return sentenca;
 }
-function decode() {
-  // seu código aqui
+
+function decode(sentenca) {
+  sentenca = sentenca.replace (/1/g, 'a');
+  sentenca = sentenca.replace (/2/g, 'e');
+  sentenca = sentenca.replace (/3/g, 'i');
+  sentenca = sentenca.replace (/4/g, 'o');
+  sentenca = sentenca.replace (/5/g, 'u');
+  return sentenca
 }
 
 module.exports = {
