0fbb117572c1e1c40be5b27271861fe58b75a274
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-06-29 22:19:00 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,10 +16,40 @@ let techsOrdem = techs.sort();
   }
 }
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+let continuar = true;
+let numerFormat = '';
+let count = 1;
+  if(number.length != 11){
+    return 'Array com tamanho incorreto.';
+  }
+  else{
+    for(let i = 0; i<number.length; i+=1){
+      for(let j = i+1; j<number.length;j+=1){
+        if(number[i] === number[j]){
+          count += 1;
+        }
+      }
+      if((number[i]<0 || number[i]>9) || count >= 3){
+        return "não é possível gerar um número de telefone com esses valores";
+      }else{
+        if(i===0){
+          numerFormat += '(' + number[i];
+        }
+        else if(i>0 && i<2 || i>2 && i<7 || i>7){
+          numerFormat += number[i];
+        }
+        else if(i === 2){
+          numerFormat += ') ' + number[i];
+        }else if(i === 7){
+          numerFormat += '-' + number[i];
+        }
+        count = 1;
+      }
+    }
+    return numerFormat;
+  }
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
