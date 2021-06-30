e0195cd4335078e73ed44c1f51c30642c36596a6
Carla <carladmaximo@gmail.com>
2021-06-30 12:53:04 -0300

    Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -14,9 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence(phraseSplit) {
-  return phraseSplit.split(" ")
-  // seu código aqui
+//Fonte do split: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254
+function splitSentence(frase) {
+  return frase.split(" ")
 }
 
 // Desafio 4
@@ -33,20 +33,28 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
+/*Recebi ajuda do Luan Peres, que me mostrou a lógica que usou. Ele usou o "array.forEach". Poŕem, eu não faço a mínima ideia de como usar isso. Tentei adaptar a mesma lógica usando o for of.*/
 let contador = 0;
 function highestCount(numeros) {
   let maior = 0;
+  let contador = 0;
   for(numero of numeros) {
-    //for (let index = 0; index === numeros.length; index += 1){
-      if (numero > maior) {
-        contador = contador + 1;
+      if (numero === numeros[0]) { // Inicia a variável maior guardando o primeiro número do array "numeros".
+        maior = numero;
       }
-    //}
+      if (numero > maior){ 
+        maior = numero; /*Bloco que, no 1º loop, faz a variável "maior" guardar o 1º numero e que inicializa a variavel "contador" guardando o valor"1". Nos seguintes loops, faz a mesma variável guardar o maior valor do array.*/
+        contador = 1;
+      } else
+      if (numero === maior) {
+        contador = contador +1; /*Bloco que garante a contagem da quantidade de vezes que o maior valor dentro do array "numeros" se repete.*/
+      }
   }
   return contador
 }
-console.log(contador)
+
 // Desafio 7
+//Fonte do Math.abs: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math
 function catAndMouse(mouse, cat1, cat2) {
   let d1 = Math.abs(mouse - cat1)
   let d2 = Math.abs(mouse - cat2)
