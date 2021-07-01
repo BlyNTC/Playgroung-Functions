89873d644f61b8f03f39756260a8c78554e9e593
Filipe Brochier <filipe.brochier@gmail.com>
2021-07-01 16:02:44 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,13 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let techArray = [];
+  array.sort();
+  if (array.length == 0) {
+    return "Vazio!";
+  } else {
+    for (let tech of array) {
+      techArray.push({tech, name});
+    }
+  } return techArray;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+//  let telephoneNumber = '';
+//  if (array.length !== 11) {
+//    return "Array com tamanho incorreto";
+//   }
+  
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
