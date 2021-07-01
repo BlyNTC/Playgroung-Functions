07e0e528417d76814bf2a683ec68d0428edf5af0
Silas <silas.oliveira8@gmail.com>
2021-07-01 14:30:06 -0300

    atanos god

--- src/challenges.js
+++ src/challenges.js
@@ -32,7 +32,7 @@ function concatName(array) {
   let resultado1 = array[0];
   let resultado2 = array[array.length - 1];
   let resultado3 = resultado2 + ',' + resultado1;
-  return resultado3;
+  return resultado3.split(" ");
 }
 
 
