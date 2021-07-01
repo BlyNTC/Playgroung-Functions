142f9c36fe379da4dd5050ac28cffbb7cbd84c2e
AlineCarolina <eualine2017@gmail.com>
2021-07-01 20:48:44 -0300

    tentativa 7489246

--- src/challenges.js
+++ src/challenges.js
@@ -1,83 +1,68 @@
 // Desafio 1
-function compareTrue(param1, param2) {
-  if (param1 === true && param2 === true) {
-    return true;
-  } else {
-    return false;
+function  compareTrue ( param1 ,  param2 )  {
+  if  ( param1  ===  true  &&  param2  ===  true )  {
+    return  true ;
+  }  else  {
+    return  false ;
   }
 }
 
 // Desafio 2
-function calcArea(base, height) {
-  return (base * height) / 2;
+function  calcArea ( base ,  height )  {
+  retorno  ( base  *  altura )  /  2 ;
 }
 
 // Desafio 3
-function splitSentence(string) {
-  let array = string.split(' ');
-  return array;
+function  splitSentence ( string )  {
+  deixe  array  =  string . dividir ( '' ) ;
+  return  array ;
 }
 
 // Desafio 4
-function concatName(arrayDeStrings) {
-  let primeiro = arrayDeStrings[0];
-  let ultimo = arrayDeStrings[arrayDeStrings.length -1];
-  return ultimo + ',' + ' ' + primeiro;
+function  concatName ( arrayDeStrings )  {
+  let  primeiro  =  arrayDeStrings [ 0 ] ;
+  let  ultimo  =  arrayDeStrings [ arrayDeStrings . comprimento  - 1 ] ;
+  return  ultimo  +  ','  +  ''  +  primeiro ;
 }
 
 // Desafio 5
-function footballPoints(wins, ties) {
-  let pontosCampeonato = (wins * 3) + ties;
-  return pontosCampeonato;
+função  footballPoints ( vitórias ,  empates )  {
+  deixe  pontosCampeonato  =  ( ganha  *  3 )  +  empates
+  voltar  pontosCampeonato
 }
 
 // Desafio 6
-function highestCount(arrayDeNumeros) {
-
+function  higherCount ( )  {
+  // seu código aqui
 }
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
+function  catAndMouse ( )  {
   // seu código aqui
 }
 
 // Desafio 8
-function fizzBuzz(numeros) {
-let numeros = [2, 15, 7, 9, 45]; 
-
-  let string = [] {
-    for (index = 0; index < numeros.length; index +1) {
-      if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0) {
-        string.push('fizzBuzz')
-      } else if (numeros[index] % 3 === 0) {
-        string.push('fizz')
-      } else if (numeros[index] % 5 === 0) {
-        string.push('buzz')
-      } else {
-        string.push('bug!')
-      }
-    }
-    return string
-  }
+function  fizzBuzz ( )  {
+  // seu código aqui
 }
 
 // Desafio 9
-function encode() {
+function  encode ( )  {
   // seu código aqui
 }
-function decode() {
+function  decode ( )  {
   // seu código aqui
 }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+módulo . exportações  =  {
+  calcArea ,
+  catAndMouse ,
+  compareTrue ,
+  concatName ,
+  decodificar ,
+  codificar ,
+  fizzBuzz ,
+  footballPoints ,
+  maximumCount ,
+  splitSentence ,
+} ;
