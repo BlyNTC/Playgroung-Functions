64231909cf410491f9a9c5d9f3a41c66aed760a0
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-07-01 20:08:43 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,32 +15,71 @@ function techList(array, name) {
   return techObject;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let checkingForRep = 0;    
+	let arrSaida = [];
+	let arrString = "";
+  if (array.length === 0) {
+    return "Array com tamanho incorreto.";
+  }
+	for (let i = 0; i < array.length; i += 1){
+		if (array[i] > 9 || array[i] < 0) {
+			return "não é possível gerar um número de telefone com esses valores";
+		} else if (array.length != 11 || array.length === 0) {
+			return "Array com tamanho incorreto.";
+		}
+		for (let k = 0; k < array.length; k += 1){
+			if (array[i] === array[k]) {
+				checkingForRep += 1;
+			}
+			if (checkingForRep >= 3) {
+				return "não é possível gerar um número de telefone com esses valores";
+			}
+		}	
+		if (checkingForRep < 3) {
+			checkingForRep = 0;
+		}
 }
+if(checkingForRep < 3) {
+	arrSaida[0] = "(";
+	arrSaida[1] = array[0];
+	arrSaida[2] = array[1];
+	arrSaida[3] = ")";
+	arrSaida[4] = " ";
+	arrSaida[5] = array[2];
+	arrSaida[6] = array[3];
+	arrSaida[7] = array[4];
+	arrSaida[8] = array[5];
+	arrSaida[9] = array[6];
+	arrSaida[10] = "-";
+	arrSaida[11] = array[7];
+	arrSaida[12] = array[8];
+	arrSaida[13] = array[9];
+	arrSaida[14] = array[10];
+	arrJoined = arrSaida.join('');
+	arrStringed = arrJoined.toString();
+	return arrStringed; 
+}
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 12
 function triangleCheck() {
