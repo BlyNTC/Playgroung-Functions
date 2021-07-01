08c56294b1e6faa91a82ff0a7611be590398d400
Lys Prestes <lysprestes@gmail.com>
2021-06-30 23:28:48 -0400

    lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,12 @@
 // Desafio 10
 function techList(list, name) {
-  // seu código aqui 
+// seu código aqui 
   let arrayTech = [];
   list.sort();
   for (let tech of list) {
     arrayTech.push({
       tech,
-      name
+      name;
     });
   }
   return arrayTech.length === 0 ? 'Vazio!' : arrayTech;
@@ -14,23 +14,23 @@ function techList(list, name) {
 
 // Desafio 11
 function generatePhoneNumber(phone) {
-  // seu código aqui
+// seu código aqui
   let counter = 0;
   if (phone.length !== 11) {
-    return "Array com tamanho incorreto.";
+    return 'Array com tamanho incorreto.';
   } else {
-    for (let j = 0; j <= phone.length; j++) {
+    for (let j = 0; j <= phone.length; j+=1) {
       counter = 0;
-      for (let i = 0; i <= phone.length; i++) { //phone[0] == 1 phone[0+1] == 1 phone[0+2] == 9
+      for (let i = 0; i <= phone.length; i+=1) { //phone[0] == 1 phone[0+1] == 1 phone[0+2] == 9
         if (phone[i] < 0 | phone[i] > 9) {
-          return "não é possível gerar um número de telefone com esses valores";
+          return 'não é possível gerar um número de telefone com esses valores';
         }
         if (phone[j] == phone[i]) {
-          counter++;
+          counter+=1;
           3
         }
         if (counter >= 3) {
-          return "não é possível gerar um número de telefone com esses valores";
+          return 'não é possível gerar um número de telefone com esses valores';
         }
       }
     }
@@ -40,7 +40,7 @@ function generatePhoneNumber(phone) {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  // seu código aqui
+// seu código aqui
   let arr = [lineA, lineB, lineC];
   arr.sort();
   if (arr[0] + arr[1] > arr[2] && arr[2] > Math.abs(arr[1] - arr[0])) {
@@ -51,12 +51,11 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate(str) {
-  // seu código aqui
+// seu código aqui
   let water = 0;
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i+=1) {
     if (isFinite(str.charAt(i))) water += Number(str.charAt(i));
   }
-  
   if (water === 1) return `${water} copo de água`;
   if (water > 1) return `${water} copos de água`;
 }
