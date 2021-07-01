569990c8d96030b2055d98fd4851f3f15da0fa73
Gabriel <gabrielnmwv@gmail.com>
2021-07-01 20:02:56 -0300

    desafios 1, 2, 4 e 5 - nao foram comittados individualmente pois havia dado git init apos clonar o repo

--- src/challenges.js
+++ src/challenges.js
@@ -1,60 +1,68 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
-
-// Desafio 2
-function calcArea() {
-  // seu código aqui
-}
-
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
-
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
-
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
-
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
-
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
-
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
-
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
-
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+function compareTrue(parametro1, parametro2){
+  if(parametro1 && parametro2 === true){
+    return true}
+    else{
+      return false}
+    }
+ 
+ 
+ // Desafio 2
+ function calcArea(base, altura) {
+  return base*altura/2
+ }
+ 
+ // Desafio 3
+ function splitSentence() {
+   // seu código aqui
+ }
+ 
+ // Desafio 4
+ function concatName(array) {
+   var primeiroNome = array[0];
+   var segundoNome = array[array.length-1];
+ 
+  return segundoNome + ', ' + primeiroNome
+ }
+ console.log(concatName(['batata', 'maca', 'cenoura']))
+ // Desafio 5
+ function footballPoints(wins, ties) {
+   return 3*wins + ties
+ }
+ 
+ 
+ // Desafio 6
+ function highestCount() {
+ 
+ }
+ 
+ // Desafio 7
+ function catAndMouse() {
+   // seu código aqui
+ }
+ 
+ // Desafio 8
+ function fizzBuzz() {
+   // seu código aqui
+ }
+ 
+ // Desafio 9
+ function encode() {
+   // seu código aqui
+ }
+ function decode() {
+   // seu código aqui
+ }
+ 
+ module.exports = {
+   calcArea,
+   catAndMouse,
+   compareTrue,
+   concatName,
+   decode,
+   encode,
+   fizzBuzz,
+   footballPoints,
+   highestCount,
+   splitSentence,
+ };
