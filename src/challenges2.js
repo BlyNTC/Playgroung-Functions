9ade167c8161415251d997617bbca53dda69b610
Andrea <andrealh08@gmail.com>
2021-07-01 17:53:41 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,11 +16,49 @@ function techList(arrayNomes, name) {
     return arraySaida
   }
 }
-let arraaay = ["React", "Jest", "HTML", "CSS", "JavaScript"];
-console.log(techList(arraaay, "Lucas"));
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let phoneNumber = ["("];
+
+  if (numbers.length != 11) {
+    return "Array com tamanho incorreto.";
+  }
+
+  for (let j = 0; j < numbers.length; j += 1) {
+    let contador = 0;
+    for (let k = 0; k < numbers.length; k += 1) {
+      if (numbers[j] === numbers[k]) {
+        contador += 1;
+      }
+    }
+    if (contador >= 3) {
+    return "não é possível gerar um número de telefone com esses valores";
+    break;
+    }
+  }
+
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    } else {
+      switch (index) {
+        case 2:
+          phoneNumber.push(") ");
+          phoneNumber.push(numbers[index]);
+          break;
+
+        case 7:
+          phoneNumber.push("-");
+          phoneNumber.push(numbers[index]);
+          break;
+
+        default:
+          phoneNumber.push(numbers[index]);
+      }
+    }
+  }
+  return phoneNumber.join('');
 }
 
 // Desafio 12
