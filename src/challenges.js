938a4e4d96d45ab996e274dee847d3fab912fb7f
Josie <seuemail@exemplo.br>
2021-07-01 17:49:57 -0300

    Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -38,16 +38,38 @@ concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])
 concatName(['foguete', 'não', 'tem', 'ré'])
 concatName(['captain', 'my', 'captain'])
 
-
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let valor1 = 3;
+  let valor2 = 1;
+   let resultadoWins = valor1 * wins;
+   let resultadoTies = valor2 * ties;
+  let resultadoFinal = resultadoWins + resultadoTies;
+    console.log(resultadoFinal);
+    return resultadoFinal;
 }
+footballPoints(14,8)
+footballPoints(1,2)
+footballPoints(0,0)
+
 
 // Desafio 6
-function highestCount() {
+function highestCount(numeros) {
   // seu código aqui
+  for (let indice = 0; indice < numeros.length; indice += 1) {
+  if ( numeros[indice] > numeros[indice + 1]) { // 1 > 6
+    let numeroMaior = numeros[indice];
+  } else {
+    numeroMaior = numeros[indice + 1];
+  }
 }
+console.log(numeroMaior);
+return numeroMaior;
+}
+highestCount([9, 1, 2, 3, 9, 5, 7])
+highestCount([0, 4, 4, 4, 9, 2, 1])
+highestCount([0, 0, 0])
 
 // Desafio 7
 function catAndMouse() {
