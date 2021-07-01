a1d9c880096acf5902ff5155977ab69025678b87
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-01 19:00:32 -0300

    desafio 11 ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,9 +20,37 @@ function techList(arr,name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(arr) {
+  let arrTel = ['(',arr[0],arr[1],') ',arr[2],arr[3],arr[4],arr[5],arr[6],'-',arr[7],arr[8],arr[9],arr[10]];
+  let print = '';
+  let repeticao = 0;
+
+  if(arr.length !== 11){
+    return "Array com tamanho incorreto.";
+  }
+  for(let k in arr){
+    for(let m in arr){
+      if(arr[k] === arr [m]){
+        repeticao +=1;
+      }
+    }
+    if(repeticao >=3){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    repeticao =0;
+  }
+  for(let i in arr){
+    if(arr[i]<0 || arr[i]>9){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+  for(let j in arrTel){
+    print += arrTel[j];
+  }
+    
+    // seu código aqui
+  return print;
+}generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1])
 
 // Desafio 12
 function triangleCheck() {
