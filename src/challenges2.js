000baf483f85033fac6b0693422f3eb7cecac95f
Danilo Menezes <danilomenezes66@gmail.com>
2021-07-01 18:07:21 -0300

    Requisito 11 Concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,12 +16,34 @@ function techList(arrayTecNames, name) {
   }
 }
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayNumbers) {
   // seu código aqui
+  let cont = 0;
+  let phoneDDD = [arrayNumbers[0], arrayNumbers[1]];
+  let phonePar1 = [arrayNumbers[2], arrayNumbers[3], arrayNumbers[4], arrayNumbers[5], arrayNumbers[6]];
+  let phonePart2 = [arrayNumbers[7],arrayNumbers[8],arrayNumbers[9],arrayNumbers[10]];
+  let phoneNumber;
+  if (arrayNumbers.length !== 11){
+    return "Array com tamanho incorreto.";
+  }
+  for (let index in arrayNumbers){
+    cont = 0;
+    for (let indexN in arrayNumbers) {
+      if (arrayNumbers[index] === arrayNumbers[indexN]){
+        cont += 1;
+      }
+    }
+    if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9 || cont >= 3){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+  phoneNumber = '(' + phoneDDD.join('') + ')' + ' ' + phonePar1.join('') + '-' + phonePart2.join('');
+  return phoneNumber; 
+  console.log(phoneNumber);
 }
-
+generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1])
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
 }
 
