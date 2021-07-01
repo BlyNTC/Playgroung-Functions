8da514bc8a9e9a1c66daaab70b283dbf35fd694d
vixemari <marianarocha308@gmail.com>
2021-07-01 19:33:19 -0300

    exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(fristValue, secondValue) {
   // seu código aqui
+    if (fristValue === true && secondValue === true0){
+      return true
+    } else { return false}
+  
 }
 
 // Desafio 2
