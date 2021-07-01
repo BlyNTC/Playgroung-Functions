22b0debf65b99adf2405737edb0672c79df68ebd
vixemari <marianarocha308@gmail.com>
2021-07-01 19:42:05 -0300

    exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,16 @@
 // Desafio 1
 function compareTrue(fristValue, secondValue) {
   // seu código aqui
-    if (fristValue === true && secondValue === true0){
+    if (fristValue === true && secondValue === true){
       return true
-    } else { return false}
+    } else {return false};
   
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height) /2;
 }
 
 // Desafio 3
