a35567718d8537c0f840b13a3e0f584b18dc61ce
Victor de Paula <victorssspaula@live.com>
2021-07-01 18:46:04 -0300

    Exercicio 11 incompleto

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,33 +10,45 @@ function techList(tech, name) {
         name: name
       })
     }
-    } else if (tech.length === 0) {
-      return 'Vazio!'
-    }
-    return obj
+  } else if (tech.length === 0) {
+    return 'Vazio!'
   }
-//   let obj = [];
-//   tech.sort();
-//   for (let i of tech) {
-//     if (tech.length === 5) {
-//       obj.push({
-//         tech: i,
-//         name: name
-//       })
-//     } 
-//   }
-//   if (tech.length === 0) {
-//     return 'Vazio!'
-//   }
-//   return obj
-// }
-
-
-
+  return obj
+}
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(nums) {
   // seu código aqui
+  let numeroTel = ''
+  let parent1 = '('
+  let parent2 = ')'
+  let traco = '-'
+  let repete = [0,1,2,3,4,5,6,7,8,9]
+  let repetido = 0
+
+  if (nums.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  } 
+  for (let i of nums) {
+    if (i < 0) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+    else if (i > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+    for (let index of repete) {
+      if (index = i)
+      repetido += 1
+    }
+    if (repetido >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+nums.splice(0, 0, parent1)
+nums.splice(3, 0, parent2)
+nums.splice(8, 0, traco)
+numeroTel = nums.toSring()
+return numeroTel
 }
 
 // Desafio 12
