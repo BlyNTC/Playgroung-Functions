6f43d3954040d4bf734bd9ee72f64fb1d92098d3
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-07-01 14:08:32 -0300

    Adiciona requsisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,37 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(technologies, name) {
+  let newList = [];
+  let tech = technologies.sort(); 
+  if (technologies.length === 0) {
+    return 'Vazio!';
+  } else {
+    for (let index = 0; index < technologies.length; index += 1) {
+    let object = {
+        tech: tech[index],
+        name: name
+      }
+      newList.push(object)
+    }
+  }
+  return newList;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
+"Lucas"));
+
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numerosTel) {
+  let Telefone = [];
+  if (numerosTel.length === 11) {
+    //todo o código caso funcione
+
+  } else if(numerosTel.length !== 11) {
+    console.log('Array com tamanho incorreto.');
+  } else if (numerosTel[index] < 0 || numerosTel[index] > 9) {
+    console.log('não é possível gerar um número de telefone com esses valores');
+  } else if () {
+
+  }
 }
 
 // Desafio 12
