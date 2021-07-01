aaa2a52d79f7de2b52927196b01d6d868af57ee6
Victor Azvdo <victor.azvdo@hotmail.com>
2021-07-01 18:35:06 -0300

    Desafios 7, 8 e 9 resolvidos

--- src/challenges.js
+++ src/challenges.js
@@ -26,7 +26,7 @@ function concatName(words) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  totalPts = (wins * 3) + (ties * 1)
+  let totalPts = (wins * 3) + (ties * 1)
   return totalPts
 }
 
@@ -43,22 +43,87 @@ function highestCount(array) {
 };
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dcat1 = Math.abs(mouse - cat1);
+  let dcat2 = Math.abs(mouse - cat2);
+  if (dcat1 < dcat2){
+    return 'cat1'
+  }else if (dcat1 > dcat2){
+    return 'cat2'
+  }else{
+    return 'os gatos trombam e o rato foge'
+  };
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i += 1){
+    if (array[i] % 3 === 0  && array[i] % 5 === 0){
+      result.push('fizzBuzz')
+    }else if(array[i] % 5 === 0){
+      result.push('buzz')
+    }else if(array[i] % 3 === 0){
+      result.push('fizz')
+    }else{
+      result.push('bug!')
+    };
+  };
+  return result
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(word) {
+  let result = ''
+    for (let i of word){
+        switch (i){
+            case 'a':
+                result += 1;
+                break;
+            case 'e':
+                result += 2;
+                break;
+            case 'i':
+                result += 3;
+                break;
+            case 'o':
+                result += 4;
+                break;
+            case 'u':
+                result += 5;
+                break;
+            default:
+                result += i
+        };
+    };
+    return result
 }
 
-function decode() {
-  // seu código aqui
+
+function decode(word) {
+  let result = ''
+    for (let i of word){
+        switch (i){
+            case '1':
+                result += 'a';
+                break;
+            case '2':
+                result += 'e';
+                break;
+            case '3':
+                result += 'i';
+                break;
+            case '4':
+                result += 'o';
+                break;
+            case '5':
+                result += 'u';
+                break;
+            default:
+                result += i
+        };
+    };
+    return result
 }
 
 module.exports = {
