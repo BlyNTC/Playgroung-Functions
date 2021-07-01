c3e308b7401509023146875160330dce746800c3
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-01 18:27:31 -0300

    Faz as alteraçoes pedidas pelo lint

--- src/challenges.js
+++ src/challenges.js
@@ -1,21 +1,20 @@
 // Desafio 1
 function compareTrue(bool1, bool2) {
-  if(bool1 === true && bool2 === true){
+  if (bool1 === true && bool2 === true) {
     return true;
-  }else{
-    return false;
   }
+  return false;
 }
 // Desafio 2
 function calcArea(base, heigth) {
-  return (base*heigth)/2;
+  return (base * heigth) / 2;
 }
 // Desafio 3
 function splitSentence(str) {
-  var split = [];
-  var aux = '';
-  for(let i = 0; i<str.length; i+=1){
-    if(str[i] === ' '){
+  let split = [];
+  let aux = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === ' ') {
       split.push(aux);
       aux = '';
     }
@@ -23,8 +22,7 @@ function splitSentence(str) {
       aux += str[i];
       split.push(aux);
       aux = '';
-    }
-    else{
+    }else {
       aux += str[i];
     }
   }
@@ -32,12 +30,12 @@ function splitSentence(str) {
 }
 // Desafio 4
 function concatName(names) {
-  let concat = names[names.length-1] + ', ' + names[0];
+  let concat = names[names.length - 1] + ', ' + names[0];
   return concat;
 }
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = 3*wins + ties;
+  let points = 3 * wins + ties;
   return points;
 }
 // Desafio 6
@@ -59,37 +57,35 @@ function highestCount(numbers) {
 }
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
- let distCat1 = Math.abs(mouse - cat1);
- let distCat2 = Math.abs(mouse -cat2);
+let distCat1 = Math.abs(mouse - cat1);
+let distCat2 = Math.abs(mouse - cat2);
 
- if(distCat1 === distCat2){
-   return 'os gatos trombam e o rato foge';
- }
- else if(distCat1 > distCat2){
-   return 'cat2';
- }
- else {
-   return 'cat1';
- }
+if (distCat1 === distCat2) {
+  return 'os gatos trombam e o rato foge';
+}
+else if (distCat1 > distCat2) {
+  return 'cat2';
+}else {
+  return 'cat1';
+}
 }
 // Desafio 8
 function fizzBuzz(numeros) {
-  let words = [];
-  for(let i = 0; i<numeros.length; i += 1){
-    if(numeros[i]%3 === 0 && numeros[i]%5 === 0){
-      words[i] = 'fizzBuzz';
-    }
-    else if(numeros[i]%3 === 0){
-      words[i] = 'fizz';
-    }
-    else if(numeros[i]%5 === 0){
-      words[i] = 'buzz';
-    }
-    else{
-      words[i] = 'bug!';
-    }
+let words = [];
+for (let i = 0; i<numeros.length; i += 1) {
+  if (numeros[i]%3 === 0 && numeros[i]%5 === 0) {
+    words[i] = 'fizzBuzz';
   }
-  return words;
+  else if (numeros[i]%3 === 0) {
+    words[i] = 'fizz';
+  }
+  else if (numeros[i]%5 === 0) {
+    words[i] = 'buzz';
+  }else {
+    words[i] = 'bug!';
+  }
+}
+return words;
 }
 // Desafio 9
 function encode(str) {
