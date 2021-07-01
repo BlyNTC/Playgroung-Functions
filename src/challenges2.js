9ac92c3aba56ced48560451247332889b88c60f3
Matheus Souza <ms.conrado25@hotmail.com>
2021-07-01 01:23:48 -0300

    requisito 11 - depois de muito tempo, conseguir finalizar - me ligue

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,7 +21,42 @@ function techList(techArray, name) {
 
 // Desafio 11
 function generatePhoneNumber(array11N) {
- 
+  let guardar = 0;
+  let valor = 0;
+  let armazenar = 0;
+  let telefone = '';
+
+  for (let conferir of array11N) {
+    for (let conferindo of array11N)
+    if (conferir === conferindo) {
+      guardar += 1;
+    } 
+    if (guardar >= 3) {
+      armazenar += guardar;
+    } guardar = 0;
+  } 
+  for (let maiorMenor of array11N) {
+    if (maiorMenor < 0 || maiorMenor > 10) {
+      valor += 1;
+    }
+  }
+  if (array11N.length > 11 || array11N.length < 11) {
+     return 'Array com tamanho incorreto.';
+  } else if (armazenar >= 3 || valor > 0) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    for (let index = 0; index < 11; index += 1) {
+      if (index === 0) {
+        telefone += '(';
+      } else if (index === 2) {
+        telefone += ') ';
+      } else if (index === 7) {
+        telefone += '-';
+      }
+      telefone += array11N[index];
+    }  return telefone;
+  }
+  
 }
 
 // Desafio 12
