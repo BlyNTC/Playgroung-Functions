8575fcb00029f250e560a68acd3825816f6d88df
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-01 19:19:40 -0300

    desafio 12 ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -50,10 +50,19 @@ function generatePhoneNumber(arr) {
     
     // seu código aqui
   return print;
-}generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1])
+}
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
+  let result = true;
+  let posA = Math.abs(lineA);
+  let posB = Math.abs(lineB);
+  let posC = Math.abs(lineC);
+
+  if(posA>posB+posC || posB>posA+posC || posC>posA+posB){
+    result = false;
+  }
+  return result;
   // seu código aqui
 }
 
