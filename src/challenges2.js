e7f5d6896f622b19566b43efc4dcc4f916601dfb
Pally <francisca.bezerr@gmail.com>
2021-07-01 19:53:20 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,13 +15,34 @@ function techList(techs1, name) {
 }
 
 
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(fone) { 
+
+   if (fone.length !== 11) {
+    return "Array com tamanho incorreto."
+  } else {
+    for (let index of fone) { 
+        if (index < 0 || index > 9) {
+        return "não é possível gerar um número de telefone com esses valores";
+      } 
+      let contador = 0;
+      for (let key of fone) {  
+        if (key === index) {
+          contador += 1; 
+        }
+        if (contador >= 3) {
+          return "não é possível gerar um número de telefone com esses valores";
+        }
+      }
+    }
+    return "(" + fone[0] + fone[1] + ")" + " " + fone[2] + fone[3] + fone[4] + fone[5] + fone[6] + "-" + fone[7] + fone[8] + fone[9] + fone[10];
+  }
 }
+
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+ 
+  
 }
 
 // Desafio 13
