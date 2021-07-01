5e8bfc25f092a5d1a5918873503d36755b256940
Carlos Afonso <carlos.flach15@gmail.com>
2021-06-30 21:50:39 -0300

    Melhorando o codigo.

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,8 @@
 // Desafio 1
-let status;
-
 function compareTrue(a, b) {
   // seu código aqui
   // se a é verdadeiro e se b é verdadeiro
+  let status;
   if (a === true && b === true) {
     status = true;
     return true;
@@ -14,59 +13,58 @@ function compareTrue(a, b) {
   }
 }
 compareTrue(false, false);
-//console.log(status);
+console.log(compareTrue(false, false));
 
 // Desafio 2
-let areaDoTriangulo = 0;
-
 function calcArea(base, height) {
+  let areaDoTriangulo = 0;
   areaDoTriangulo = (base * height) / 2;
   return areaDoTriangulo;
   // seu código aqui
 }
 calcArea(51, 1);
-//console.log(areaDoTriangulo);
-
+console.log(calcArea(51, 1));
 
 // Desafio 3
-let array;
-
 function splitSentence(text) {
   // seu código aqui
+  let array;
   array = text.split(" ", 3);
   return array;
 }
 splitSentence("foguete");
-//console.log(array);
+console.log(splitSentence("foguete"));
 
 /* Consultei o seguinte site para conseguir realizar esse desafio 3.
    link: https://blog.betrybe.com/javascript/javascript-split/ */
 
 // Desafio 4
-let myString = " ";
+
 
 function concatName(arrays) {
   // seu código aqui
+  let myString = " ";
   myString = arrays[arrays.length - 1];
   myString = myString + ", " + arrays[0];
   return myString;
 }
 concatName(['captain', 'my', 'captain']);
-//console.log(myString);
+console.log(concatName(['captain', 'my', 'captain']));
 
 
 // Desafio 5
-let numero_de_pontos = 0;
+
 
 function footballPoints(wins, ties) {
   // seu código aqui
+  let numero_de_pontos = 0;
   const vitoria = 3;
   const empate = 1;
   numero_de_pontos = wins * vitoria + ties * empate;
   return numero_de_pontos;
 }
 footballPoints(14, 8);
-//console.log(numero_de_pontos);
+console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount(arraynumbers) {
@@ -82,56 +80,54 @@ function highestCount(arraynumbers) {
   return cont;
 }
 highestCount([9, 1, 2, 3, 9, 5, 7]);
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
-let situacaoGatos;
-let posicaoRato;
-let posicaoGato1;
-let posicaoGato2;
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let situacaoGatos;
+  let posicaoRato;
+  let posicaoGato1;
+  let posicaoGato2;
   posicaoRato = Math.abs(mouse);
   posicaoGato1 = Math.abs(mouse - cat1);
-  posicaoGato2 = Math.abs(mouse -cat2);
-  
-  if(posicaoGato1 == 6 & posicaoGato2 == 12){
+  posicaoGato2 = Math.abs(mouse - cat2);
+
+  if (posicaoGato1 == 6 & posicaoGato2 == 12) {
     situacaoGatos = "cat1";
     return situacaoGatos;
-  }
-  else if (posicaoGato1 == 3 & posicaoGato2 == 2){
+  } else if (posicaoGato1 == 3 & posicaoGato2 == 2) {
     situacaoGatos = 'cat2';
     return situacaoGatos;
-  }
-  else if (posicaoGato2 == posicaoGato1){
+  } else if (posicaoGato2 == posicaoGato1) {
     situacaoGatos = "os gatos trombam e o rato foge";
     return situacaoGatos;
   }
 }
-catAndMouse(0,3,3);
-//console.log(situacaoGatos);
+catAndMouse(0, 3, 3);
+console.log(catAndMouse(0, 3, 3));
 
 
 // Desafio 8
 function fizzBuzz(arrayFizzBuzz) {
   // seu código aqui
   let arrayPush = [];
-  for (let idx = 0; idx < arrayFizzBuzz.length; idx += 1){
-      if(arrayFizzBuzz[idx] % 3 === 0 && arrayFizzBuzz[idx] % 5 === 0){
-        arrayPush[idx] = 'fizzBuzz';
-      }
-      else if (arrayFizzBuzz[idx] % 3 === 0){
-        arrayPush[idx] = 'fizz';
-      }
-      else if (arrayFizzBuzz[idx] % 5 === 0){
-        arrayPush[idx] = 'buzz';
-      }
-      else {
-        arrayPush[idx] = "bug!";
-      }
+  for (let idx = 0; idx < arrayFizzBuzz.length; idx += 1) {
+    if (arrayFizzBuzz[idx] % 3 === 0 && arrayFizzBuzz[idx] % 5 === 0) {
+      arrayPush[idx] = 'fizzBuzz';
+    } else if (arrayFizzBuzz[idx] % 3 === 0) {
+      arrayPush[idx] = 'fizz';
+    } else if (arrayFizzBuzz[idx] % 5 === 0) {
+      arrayPush[idx] = 'buzz';
+    } else {
+      arrayPush[idx] = "bug!";
+    }
   }
   return arrayPush;
 }
 fizzBuzz([7, 9]);
+console.log(fizzBuzz([7, 9]));
+
 
 
 // Desafio 9
