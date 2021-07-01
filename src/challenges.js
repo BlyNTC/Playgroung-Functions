0b365e8ac6781962e9dc747a9f19fd5e961621bc
Leonidas Junior <leonautcco@gmail.com>
2021-07-01 18:31:25 -0300

    Solucao Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,20 +1,21 @@
 // Desafio 1
 function compareTrue(a, b) {
   if (a === true && b === true) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
 }
+
 // Desafio 2
 function calcArea(base, height) {
   let area = (base * height) / 2
-  return area;
-  // seu código aqui
+  return area
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(array) {
+  return array.split(" ")
   // seu código aqui
 }
 
