3b945326c526e68782b8e3fd715daecbe997b7fd
Victor de Paula <victorssspaula@live.com>
2021-07-01 17:53:10 -0300

    Exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,19 +1,38 @@
 // Desafio 10
 function techList(tech, name) {
   // seu código aqui
-  let obj = []
-  for (let key of tech) {
-    if (tech.length >= 5) {
-      obj.push.sort({
-        tech: key,
+  let obj = [];
+  tech.sort();
+  if (tech.length === 5) {
+    for (let i of tech) {
+      obj.push({
+        tech: i,
         name: name
       })
-    } else if (tech.length === 0) {
-      obj.push ('Vazio!')
     }
+    } else if (tech.length === 0) {
+      return 'Vazio!'
+    }
+    return obj
   }
-  return obj
-}
+//   let obj = [];
+//   tech.sort();
+//   for (let i of tech) {
+//     if (tech.length === 5) {
+//       obj.push({
+//         tech: i,
+//         name: name
+//       })
+//     } 
+//   }
+//   if (tech.length === 0) {
+//     return 'Vazio!'
+//   }
+//   return obj
+// }
+
+
+
 
 // Desafio 11
 function generatePhoneNumber() {
