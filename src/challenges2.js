9688ae941ca02f0fe83fad18c0a8f2e3589a35e6
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-07-02 14:53:02 -0300

    resquisito 11 - funcao de numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,10 +16,9 @@ function techList(array, name) {
   if (array.length === 0) {
     answer = 'Vazio!';
   }
-  else{
+  else {
     for (let elem of array) {
-      let object = 
-      {
+      let object = {
         'tech': elem,
         'name': name
       };
@@ -32,9 +31,55 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function checkArray(array) {
+  let answer;
+  let qt = 0;
+  if (array.length !== 11) {
+    answer = 0;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    for (let b = 0; b < array.length; b += 1) {
+      if (array[i] === array[b] && i !== b ){
+        qt += 1;
+      }
+    }
+    if (array[i] < 0 || array[i] > 9 || qt >= 3){
+      answer = 1;
+    }
+  }
+  return answer;
 }
+
+function generatePhoneNumber(array) {
+  let answer;
+  let phoneNumber = '';
+  let returnCheckArray = checkArray(array);
+  if (returnCheckArray === 0) {
+    answer = 'Array com tamanho incorreto.';
+  }
+  else if (returnCheckArray === 1){
+    answer = 'não é possível gerar um número de telefone com esses valores';
+  }
+  else {
+    for (let i = 0; i < array.length; i += 1) {
+      if (i === 0) {
+        phoneNumber += '(' + array[i];
+      } else if (i === 1) {
+        phoneNumber += array[i] + ') ';
+      } else if (i === 6) {
+        phoneNumber += array[i] + '-';
+      } else {
+        phoneNumber += array[i];
+      }
+    } 
+    answer = phoneNumber;
+  }
+
+  return answer;
+}
+
+
 
 // Desafio 12
 function triangleCheck() {
