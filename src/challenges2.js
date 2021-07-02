2948f2c72ae7a36f80e90254af1855924e927e5b
Pedro Barreto <pbdeoliveira@gmail.com>
2021-07-02 17:25:38 -0300

    bonus 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,14 +12,50 @@ function techList(skill, name) {
 techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas');
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(telephoneArray) {
+
+// let telephoneNumber = 2193458768;
+
+// for ( i = 0; i < telephoneArray.length; i += 1)
+
+// if ( telephoneNumber  )
+
+
 }
 
+generatePhoneNumber([ 1, 2, 3, 4, 5, 6 ,7, 8, 9, 0, 1 ]);
+
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+
+  
+
+  if ( lineA > lineB + lineC  || lineB > lineA + lineC || lineC > lineA + lineB ) {
+  
+    return false;
+  
+  }
+
+  else if ( lineA < (lineB + lineC)  ===  true && lineA >  Math.abs( lineB - lineC) === true ) {
+
+    return true;
+
+  } else if ( lineB < (lineC + lineA)  ===  true && lineB >  Math.abs( lineA - lineC) === true ) { 
+
+    return true; 
+
+  } else if  ( lineC < (lineB + lineA)  ===  true && lineC >  Math.abs( lineB - lineC) === true ) { 
+
+    return true; 
+
+   }  else {
+
+    return false;
+  }
+
 }
+
+triangleCheck( 30, 23, 10);
 
 // Desafio 13
 function hydrate() {
