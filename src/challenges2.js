28365f3e929ff6b0f1d8bd20db6d86992083f823
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-02 12:57:43 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,20 +20,34 @@ console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  //   let phoneNumber;
-//   console.log(phoneNumber)
-//   let response;
-//   for (let index in numbers) {
-//     if (numbers.length !== 11) {
-//       response = 'Array com tamanho incorreto';
-//     } else if (numbers[index] < 0 || numbers[index] > 9) {
-//       response = 'não é possível gerar um número de telefone com esses valores';
-//     } else if {
-//       phoneNumber = Math.ceil(Math.random() * numbers[index])}
-//     return response;
-//   }
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  
+  let count = 0;
+  for (let index of numbers) {
+  if (index < 0 || index > 9) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
 }
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]))
+
+  for (let index of numbers) {
+    for (let index2 of numbers) {
+      if (index === index2) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } else {
+      count = 0;
+    }
+  }
+
+  return '(' + numbers[0] + numbers[1] + ') ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10];
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
