c178a16652fcc943f3b44784199f656d1afd850f
Matheus Alves <malves224@gmail.com>
2021-07-01 21:47:56 -0300

    concerta problema no desafio 10 para imprimir 'vazio'

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,21 @@
 // Desafio 10
 function techList(array, stringName) {
   let arrayOutPut = [];
-  if (array === undefined) {
-    return "Vazio!"
-  } else {
     for (let index of array) {
       let objeto = {
         tech: index,
-        nome: stringName
+        nme: stringName
       }
       arrayOutPut.push(objeto)
-    }
+    } if (array.length === 0){
+      return "Vazio!";
+    } else {
     return arrayOutPut.sort(function (a, b) {
       return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0) // encontrei essa solução para ordenanar objetos em um array no site https://www.edsonemiliano.com.br/blog
     })
   }
 }
-console.log(techList());
+console.log(techList(["React",], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
