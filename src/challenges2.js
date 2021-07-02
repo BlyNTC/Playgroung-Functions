ada921176a7d069bfe89804ec466783b1790b563
Gabriel Franklin <franklingabriel158@gmail.com>
2021-07-01 22:17:44 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -27,10 +27,24 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA,lineB,lineC) {
+  let trianguloA = lineB + lineC;
+  let trianguloB = lineC + lineA;
+  let trianguloC = lineA + lineB;
+  let diferenceA = Math.abs(lineB - lineC);
+  let diferenceB = Math.abs(lineC - lineA);
+  let diferenceC = Math.abs(lineA - lineB);
+  if(lineA < trianguloA && lineA > diferenceA){
+    return true;// seu código aqui
+  } else if(lineB < trianguloB && lineB > diferenceB){
+  return true;
+  } else if(lineC < trianguloC && lineC > diferenceC){
+    return true;
+  } else {
+    return false;
+  }
 
+}
 // Desafio 13
 function hydrate() {
   // seu código aqui
@@ -42,3 +56,4 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
