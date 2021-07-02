43c33f3ca4f9193e1a3881a2455f2b504b4fc437
Leonidas Junior <leonautcco@gmail.com>
2021-07-02 20:33:24 -0300

    Correcao desafio 7 eslint

--- src/challenges.js
+++ src/challenges.js
@@ -38,13 +38,12 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (Math.abs(mouse - cat1) < Math.abs(mouse-cat2)) {
-    return 'cat1'
-  } if (Math.abs(mouse - cat1) > Math.abs(mouse-cat2)) {
-    return 'cat2'
-  } else {
-    return 'os gatos trombam e o rato foge'
-  }
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  } if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
+    return 'cat2';
+  } 
+    return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
