7e66534fa4312f5e53d5b4e967a036739db19213
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-07-02 14:39:04 -0300

    Lista o array do parametro tecnologia, acresenta objetos e retorna em ordem crescente

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tecnologia, name) {
+  tecnologia.sort();
+  for (let i = 0; i < tecnologia.length; i += 1) {
+    tecnologia[i] = {
+      tech: tecnologia[i],
+      name: name
+    };
+  }
+  if (tecnologia.length > 0) {
+    return tecnologia;
+  }
+  return 'Vazio!';
 }
 
 // Desafio 11
