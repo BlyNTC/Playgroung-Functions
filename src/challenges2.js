6214def9c4022951612ed36f805f2159ed387020
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-02 13:12:25 -0300

    Bonus 11 adicionado

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,19 +4,50 @@ function techList(tech, name) {
   for (let index = 0; index < tech.length; index += 1) {
     tech[index] = {
       tech: tech[index],
-      name: name
-    }
+      name: name,
+    };
   }
   if (tech == '') {
     return 'Vazio!';
   } else {
-    return (tech);
+    return tech;
   }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayTelefone) {
+  if (arrayTelefone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let repeticao = 0;
+  for (let i = 0; i < arrayTelefone.length; i += 1) {
+    for (let i2 = 0; i2 < arrayTelefone.length; i2 += 1) {
+      if (arrayTelefone[i] == arrayTelefone[i2]) {
+        repeticao += 1;
+      }
+    }
+    if ((repeticao >= 3) || (arrayTelefone[i] > 9) || (arrayTelefone[i] < 0)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    repeticao = 0;
+  }
+  let phoneNumber = "";
+  for (let i3 = 0; i3 < arrayTelefone.length; i3 += 1) {
+    if (phoneNumber.length == 0) {
+      phoneNumber += "(";
+    }
+    if (phoneNumber.length == 3) {
+      phoneNumber += ")";
+    }
+    if (phoneNumber.length == 4) {
+      phoneNumber += " ";
+    }
+    if (phoneNumber.length == 10) {
+      phoneNumber += "-";
+    }
+    phoneNumber += arrayTelefone[i3];
+  }
+  return phoneNumber;
 }
 
 // Desafio 12
