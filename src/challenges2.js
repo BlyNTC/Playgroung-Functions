4c9b05f7115d05f308b22f00776109138c58934f
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-07-02 15:51:45 -0300

    completa 11º funcao

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,18 +15,93 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(listOfNumbers) {
   // seu código aqui
+  let result = 0, listConvertedForString = '', keepGoing = false;
+
+
+  if (listOfNumbers.length != 11) {
+
+    result = "Array com tamanho incorreto.";
+
+    return result;
+
+  } else {
+
+    for (let numberFirstCheck of listOfNumbers) {
+      let couterOfRepeatNumbers = 0;
+  
+      if (numberFirstCheck < 0 || numberFirstCheck > 9) {
+
+        result = "não é possível gerar um número de telefone com esses valores";
+
+        return result;
+      } else {
+
+        for (let numberSecondCheck of listOfNumbers) {
+  
+          if (numberFirstCheck === numberSecondCheck) {
+            couterOfRepeatNumbers += 1;
+          }
+    
+        }
+    
+        if (couterOfRepeatNumbers >= 3) {
+
+          result = "não é possível gerar um número de telefone com esses valores";
+
+          return result;
+
+        }
+
+      }      
+  
+    }
+    
+  } 
+
+
+  for (let index = 0; index < listOfNumbers.length; index += 1) {
+
+    if (index == 0) {
+
+      listConvertedForString += '(';
+      listConvertedForString += `${listOfNumbers[index]}`;
+
+    } else if (index == 2) {
+
+      listConvertedForString += ')';
+      listConvertedForString += ` `;
+      listConvertedForString += `${listOfNumbers[index]}`;
+
+    } else if (index == 7) {
+
+      listConvertedForString += '-';
+      listConvertedForString += `${listOfNumbers[index]}`;
+      
+    } else {
+
+      listConvertedForString += `${listOfNumbers[index]}`;
+
+    }
+    
+  }
+
+  result = listConvertedForString;
+
+  return result;
+
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
   // seu código aqui
+
 }
 
 module.exports = {
