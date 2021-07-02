7d7421a5c35040102434d8355f6d6796e7100bbb
RicardoAlFerreira <dieselferreira@gmail.com>
2021-07-02 15:56:57 -0300

    Exercicios 9 e 12 concluidos

--- src/challenges.js
+++ src/challenges.js
@@ -81,11 +81,42 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let array = string.split("");
+  for (let index in array){
+    if(array[index]=== 'a') {
+      array[index] = 1;
+    } else if (array[index] === 'e') {
+      array[index] = 2;
+    } else if (array[index] ==='i') {
+      array[index] = 3;
+    } else if (array[index] === 'o') {
+      array[index] = 4;
+    } else if (array[index] === 'u') {
+      array[index] = 5;
+    }
+  }
+  let codificado = array.join("")
+  return codificado;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let array = string.split("");
+  for (let index in array){
+    if(array[index]=== '1') {
+      array[index] = 'a';
+    } else if (array[index] === '2') {
+      array[index] = 'e';
+    } else if (array[index] ==='3') {
+      array[index] = 'i';
+    } else if (array[index] === '4') {
+      array[index] = 'o';
+    } else if (array[index] === '5') {
+      array[index] = 'u';
+    }
+  }
+  let decodificado = array.join("")
+  return decodificado;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array,name) {
+  var listaDeTec = [];
+  
+  for (let index = 0; index < array.length; index += 1) {
+    let objeto = {
+      tech: array[index],
+      name: name
+    }
+    listaDeTec.push(objeto);
+  }  
+  if (listaDeTec == "") {
+    return "Vazio!";
+  } else {
+    return listaDeTec;
+  }
+
 }
+console.log(techList("React", "Jest", "HTML", "CSS", "JavaScript"));
+
 
 // Desafio 11
 function generatePhoneNumber() {
@@ -9,9 +25,18 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-
+function triangleCheck(lineA,lineB,lineC) {
+  if (Math.abs((lineA - lineB || lineA - lineB )) < lineC && lineC < (lineA + lineB)) {
+    return true;
+  } else if (Math.abs((lineA - lineC || lineA - lineC)) < lineB && lineB < (lineA + lineC)){
+    return true;
+  } else if (Math.abs((lineB - lineC || lineB - lineC)) < lineA && lineA < (lineB + lineC)){
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 // Desafio 13
 function hydrate() {
