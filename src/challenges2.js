7c676cfb5ad206c22b0c9fa0964b5e6787649c65
Marco <marcoantonio99d@gmail.com>
2021-07-02 17:31:18 -0300

    exercicio 12 resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -38,8 +38,30 @@ function generatePhoneNumber(phoneNumber) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let soma1 = lineA + lineB;
+  let soma2 = lineA + lineC;
+  let soma3 = lineB + lineC;
+  let subAbs1 = Math.abs(lineA - lineB);
+  let subsAbs2 = Math.abs(lineA - lineC);
+  let subAbs3 = Math.abs(lineB - lineC);
+  let ladoAValido;
+  let ladoBValido;
+  let ladoCValido;
+if (lineA < soma3 && lineA > subAbs3) {
+  ladoAValido = true;
+}
+if (lineB < soma2 && lineB > subsAbs2) {
+  ladoBValido = true;
+}
+if (lineC < soma1 && lineC > subAbs1) {
+  ladoCValido = true;
+}
+if (ladoAValido === true || ladoBValido === true || ladoCValido === true) {
+  return true;
+} else {
+  return false;
+}
 }
 
 // Desafio 13
