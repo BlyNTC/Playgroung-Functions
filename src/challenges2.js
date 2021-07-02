a9ad51ca626ff5facaf61b51964f356fe69c5f09
Matheus Alves <malves224@gmail.com>
2021-07-01 21:06:40 -0300

    resolve requisito 10 do projeto

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, stringName) {
+  let arrayOutPut = [];
+  for (let index of array) {
+    let objeto = {
+      tech: index,
+      nome: stringName
+    }
+    arrayOutPut.push(objeto)
+  }
+  console.log(arrayOutPut.sort(function (a, b) {
+    return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0) // encontrei essa solução para ordenanar objetos em um array no site https://www.edsonemiliano.com.br/blog
+  }))
 }
+techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
+  "Lucas");
 
 // Desafio 11
 function generatePhoneNumber() {
