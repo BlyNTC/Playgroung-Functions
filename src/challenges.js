5c0a0f959ecbad939870354e641edadc835c3c7c
Josie <seuemail@exemplo.br>
2021-07-02 12:43:04 -0300

    Correção de indentação

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,24 @@
 // Desafio 1
-function compareTrue(a,b) { //a=false b=true
-  if (a === true && b === true) { 
+function compareTrue(a, b) {
+  if (a === true && b === true) {
     return true;
   }
   return false;
-  }
+}
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  return (base * height)/2
+  return (base * height) / 2;
 }
-calcArea(10, 50)
-calcArea(5,2)
-calcArea(51,1)
-
+calcArea(10, 50);
+calcArea(5, 2);
+calcArea(51, 1);
 
 // Desafio 3
 function splitSentence(frase) {
   // seu código aqui
-  
-return frase.split(' ');
+  return frase.split(' ');
 }
 splitSentence('go Trybe')
 splitSentence('vamo que vamo')
@@ -31,7 +29,7 @@ function concatName(palavras) {
   // seu código aqui
   let ultimaPalavra = palavras[palavras.length - 1];
   let primeiraPalavra = palavras[0];
-  
+
   return ultimaPalavra + ', ' + primeiraPalavra;
 }
 concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])
@@ -46,12 +44,12 @@ function footballPoints(wins, ties) {
   let resultadoWins = valor1 * wins;
   let resultadoTies = valor2 * ties;
   let resultadoFinal = resultadoWins + resultadoTies;
-    
-    return resultadoFinal;
+
+  return resultadoFinal;
 }
-footballPoints(14,8)
-footballPoints(1,2)
-footballPoints(0,0)
+footballPoints(14, 8)
+footballPoints(1, 2)
+footballPoints(0, 0)
 
 // Desafio 6
 function highestCount(numeros) {
@@ -63,12 +61,12 @@ function highestCount(numeros) {
       numeroMaior = numeros[indice];
       repeticaoNumeroMaior = 0;
     }
-    if (numeros[indice] === numeroMaior){
+    if (numeros[indice] === numeroMaior) {
       repeticaoNumeroMaior = repeticaoNumeroMaior + 1;
     }
   }
 
-return repeticaoNumeroMaior;
+  return repeticaoNumeroMaior;
 }
 highestCount([9, 1, 2, 3, 9, 5, 7])
 highestCount([0, 4, 4, 4, 9, 2, 1])
@@ -77,41 +75,41 @@ highestCount([0, 0, 0])
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if (cat2 < cat1){
-  
+  if (cat2 < cat1) {
+
     return 'cat2';
-  } 
-  if (cat1 < cat2){
-  
+  }
+  if (cat1 < cat2) {
+
     return 'cat1';
-  } 
+  }
   if (cat1 == cat2) {
-    
+
     return 'os gatos trombam e o rato foge';
   }
 }
 catAndMouse(0, 3, 2)
-catAndMouse(0, 6 , 12)
+catAndMouse(0, 6, 12)
 catAndMouse(0, 0, 0)
 
 // Desafio 8
 function fizzBuzz(arrays) {
   // seu código aqui
-  let fizzBuzz= []
+  let fizzBuzz = []
   for (let i = 0; i < arrays.length; i += 1) {
     if (arrays[i] % 3 === 0 && arrays[i] % 5 == 0) {
       fizzBuzz.push('fizzBuzz');
-    } else if (arrays[i] % 3 == 0){
-      fizzBuzz.push('fizz'); 
+    } else if (arrays[i] % 3 == 0) {
+      fizzBuzz.push('fizz');
     } else if (arrays[i] % 5 == 0) {
       fizzBuzz.push('buzz')
     } else {
       fizzBuzz.push('bug')
     }
 
-    }
-    
-    return fizzBuzz;
+  }
+
+  return fizzBuzz;
 }
 fizzBuzz([2, 15, 7, 9, 45])
 fizzBuzz([7, 9])
@@ -125,15 +123,15 @@ function encode(string) {
   function encode(string) {
     let codigo = '';
     let vogais = ['a', 'e', 'i', 'o', 'u'];
-  
+
     for (let i = 0; i < string.length; i += 1) {
       if (vogais.includes(string[i])) {
-      codigo += (vogais.indexOf(string[i]) + 1);
+        codigo += (vogais.indexOf(string[i]) + 1);
       } else {
         codigo += string[i];
       }
     }
-    
+
     return codigo;
   }
   console.log(encode('hi there'));
@@ -144,7 +142,7 @@ function decode() {
     let string = '';
     let lista = ['1', '2', '3', '4', '5'];
     let vogais = ['a', 'e', 'i', 'o', 'u'];
-  
+
     for (let i = 0; i < code.length; i += 1) {
       if (lista.includes(code[i])) {
         string += vogais[lista.indexOf(code[i])];
