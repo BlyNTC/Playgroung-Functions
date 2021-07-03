911984f6e5f3264170aa8bcf82cdd4654bc7716d
Victor Ryan <victorryandias@gmail.com>
2021-07-03 20:04:59 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,13 +17,33 @@ return array;
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeroDeTelefone) {
+  let resultado = "(" + numeroDeTelefone[0] + numeroDeTelefone[1] + ") " + numeroDeTelefone[2] + numeroDeTelefone[3] + numeroDeTelefone[4] + numeroDeTelefone[5] + numeroDeTelefone[6] + "-" + numeroDeTelefone[7] + numeroDeTelefone[8] + numeroDeTelefone[9] + numeroDeTelefone[10];
+  if(numeroDeTelefone.length !== 11){
+  return "Array com tamanho incorreto."
 }
+  for(let i = 0; i < numeroDeTelefone.length; i += 1){
+  let resultado2 = 0;
+    if(numeroDeTelefone[i] < 0 || numeroDeTelefone[i] > 9){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  for(let i2 = 1; i2 < numeroDeTelefone.length; i2 += 1){
+    if(numeroDeTelefone[i] === numeroDeTelefone[i2]){
+    resultado2 += 1;
+    }
+    if(resultado2 >= 3){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    }
+  }
+  return resultado;   
+  }
+
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+  // 
+
 }
 
 // Desafio 13
