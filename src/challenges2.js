6a7bf982a12629ae78ab3bbb59e9a0c31258f813
Maurício Leite <mauricioleite1@gmail.com>
2021-07-03 18:06:41 -0300

    Requisito 10.5

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,34 +1,19 @@
 // Desafio 10
 
-// function techList(techs, name) {
-//   let list = [];
-//   let arFinal = [];
-//   if (techs.length === 5) {
-//     for (let i = 0; i < techs.length; i += 1) {
-//       list.push(techs[i]);
-//     }
-//     let techArray = list.sort();   
-//     for (let iA = 0; iA < techArray.length; iA += 1) {
-//       arFinal.push({ tech : techArray[iA], name });
-//     }
-//     return arFinal;
-//   } if (techs.length === 0) {
-//     return 'Vazio!';
-//   }
-// }
-
-// // Desafio 10.1
 function techList(techs, name) {
-  let techSort = techs.sort();
-  let finalList = [];
+  let list = [];
+  let arFinal = [];
   if (techs.length === 5) {
-    for (let i = 0; i < techSort.length; i += 1) {
-      finalList.push({ tech: techSort[i], name });
+    for (let i = 0; i < techs.length; i += 1) {
+      list.push(techs[i]);
     }
-    return finalList;
+    let techArray = list.sort();
+    for (let iA = 0; iA < techArray.length; iA += 1) {
+      arFinal.push({ tech : techArray[iA], name });
+    }
+    return arFinal;
   } if (techs.length === 0) {
-    finalList = 'Vazio';
-    return finalList;
+    return 'Vazio!';
   }
 }
 
