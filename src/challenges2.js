53fd8815d64107c4d2702d26df2c75d095cfbe81
Maurício Leite <mauricioleite1@gmail.com>
2021-07-03 18:03:09 -0300

    Requisito 10.4

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,29 +1,36 @@
 // Desafio 10
 
-function techList(techs, name) {
-  let list = [];
-  let arFinal = [];
-  if (techs.length === 5) {
-    for (let i = 0; i < techs.length; i += 1) {
-      list.push(techs[i]);
-    }
-    let techArray = list.sort();   
-    for (let iA = 0; iA < techArray.length; iA += 1) {
-      arFinal.push({ tech : techArray[iA], name })
-    }
-    return arFinal;
-  } if (techs.length === 0) {
-    return 'Vazio!';
-  }
-}
-
-// // Desafio 10.1
 // function techList(techs, name) {
-//   let techSort = techs.sort();
-
+//   let list = [];
+//   let arFinal = [];
+//   if (techs.length === 5) {
+//     for (let i = 0; i < techs.length; i += 1) {
+//       list.push(techs[i]);
+//     }
+//     let techArray = list.sort();   
+//     for (let iA = 0; iA < techArray.length; iA += 1) {
+//       arFinal.push({ tech : techArray[iA], name });
+//     }
+//     return arFinal;
+//   } if (techs.length === 0) {
+//     return 'Vazio!';
+//   }
 // }
 
-// techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Maurício');
+// // Desafio 10.1
+function techList(techs, name) {
+  let techSort = techs.sort();
+  let finalList = [];
+  if (techs.length === 5) {
+    for (let i = 0; i < techSort.length; i += 1) {
+      finalList.push({ tech: techSort[i], name });
+    }
+    return finalList;
+  } if (techs.length === 0) {
+    finalList = 'Vazio';
+    return finalList;
+  }
+}
 
 // Desafio 11
 function generatePhoneNumber() {
