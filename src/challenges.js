28ca5f3264b6bafdda5124e3cdbff967491ed83a
Yan Brasileiro <yan.brasileiro@gmail.com>
2021-07-02 21:06:38 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,12 +13,13 @@ function calcArea(base,height) {
   // seu código aqui
     let area = base * height / 2;
     return area;
-  
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(frase) {
   // seu código aqui
+  let quebraFrase = frase.split(" ");
+  return quebraFrase;
 }
 
 // Desafio 4
