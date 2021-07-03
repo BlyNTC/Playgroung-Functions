628d1150f234389b45a9dece3c250c3f120213c9
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-07-02 23:12:50 -0300

    testando exercicio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -38,16 +38,32 @@ function triangleCheck(lineA, lineB, lineC) {
   let bMenosC = Math.abs(lineB - lineC);
   let aMenosC = Math.abs(lineA - lineC);
   let aMenosB = Math.abs(lineA - lineB);
-  if (bMenosC < lineA && lineA < lineB + lineC || aMenosC < lineB && lineB < lineA + lineC || aMenosB < lineC && lineC < lineA + lineB) {
+  if (
+    (bMenosC < lineA && lineA < lineB + lineC) ||
+    (aMenosC < lineB && lineB < lineA + lineC) ||
+    (aMenosB < lineC && lineC < lineA + lineB)
+  ) {
     return true;
   }
   return false;
-} console.log(triangleCheck(10, 13, 2));
+}
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let retornaString = '';
+  let totalNumero = string.match(/\d+/g);
+  if (totalNumero == 1) {
+    retornaString = retornaString.concat(totalNumero, ' copo de água');
+  }
+  for (let i = 0; i < totalNumero.length; i += 1) {
+    if (totalNumero[i] >= 1) {
+      totalNumero += totalNumero[i];
+      retornaString = retornaString.concat(totalNumero, ' copos de água');
+    }
+  }
+  return retornaString;
 }
+console.log(hydrate('1 cerveja e 4 cerveja'));
 
 module.exports = {
   generatePhoneNumber,
