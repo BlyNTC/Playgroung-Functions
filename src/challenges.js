bc62c53e139bebf587369561d80a8e84710ed631
Josie <seuemail@exemplo.br>
2021-07-03 19:32:03 -0300

    Correções finais

--- src/challenges.js
+++ src/challenges.js
@@ -11,18 +11,18 @@ function calcArea(base, height) {
   // seu código aqui
   return (base * height) / 2;
 }
-calcArea(10, 50);
-calcArea(5, 2);
-calcArea(51, 1);
+//calcArea(10, 50);
+//calcArea(5, 2);
+//calcArea(51, 1);
 
 // Desafio 3
 function splitSentence(frase) {
   // seu código aqui
   return frase.split(' ');
 }
-splitSentence('go Trybe');
-splitSentence('vamo que vamo');
-splitSentence('foguete');
+//splitSentence('go Trybe');
+//splitSentence('vamo que vamo');
+//splitSentence('foguete');
 
 // Desafio 4
 function concatName(palavras) {
@@ -30,11 +30,11 @@ function concatName(palavras) {
   let ultimaPalavra = palavras[palavras.length - 1];
   let primeiraPalavra = palavras[0];
 
-  return ultimaPalavra + (', ') + primeiraPalavra;
+  return ultimaPalavra + ', ' + primeiraPalavra;
 }
-concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
-concatName(['foguete', 'não', 'tem', 'ré']);
-concatName(['captain', 'my', 'captain']);
+//concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
+//concatName(['foguete', 'não', 'tem', 'ré']);
+//concatName(['captain', 'my', 'captain']);
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -47,120 +47,97 @@ function footballPoints(wins, ties) {
 
   return resultadoFinal;
 }
-footballPoints(14, 8);
-footballPoints(1, 2);
-footballPoints(0, 0);
+//footballPoints(14, 8);
+//footballPoints(1, 2);
+//footballPoints(0, 0);
 
 // Desafio 6
 function highestCount(numeros) {
   // seu código aqui
-  let numeroMaior = 0;
+  let numeroMaior = -Infinity;
   let repeticaoNumeroMaior = 0;
   for (let indice = 0; indice < numeros.length; indice += 1) {
-    if (numeros[indice] > numeroMaior) {
+    console.log(numeros[indice]);
+    if (numeroMaior < numeros[indice]) {
       numeroMaior = numeros[indice];
       repeticaoNumeroMaior = 0;
     }
-
     if (numeros[indice] === numeroMaior) {
-      repeticaoNumeroMaior = repeticaoNumeroMaior + 1;
+      repeticaoNumeroMaior += 1;
     }
   }
-
   return repeticaoNumeroMaior;
 }
 highestCount([9, 1, 2, 3, 9, 5, 7]);
-highestCount([0, 4, 4, 4, 9, 2, 1]);
-highestCount([0, 0, 0]);
+//highestCount([0, 4, 4, 4, 9, 2, 1]);
+//highestCount([0, 0, 0]);
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if (cat2 < cat1) {
-
-    return 'cat2';
-  }
-  if (cat1 < cat2) {
-
+  let distanciaCat1ToMouse = Math.abs(mouse - cat1);
+  let distanciaCat2ToMouse = Math.abs(mouse - cat2);
+  if(distanciaCat1ToMouse < distanciaCat2ToMouse){
     return 'cat1';
   }
-  if (cat1 == cat2) {
-
+  if(distanciaCat2ToMouse < distanciaCat1ToMouse) {
+    return 'cat2';
+  }
+  if(distanciaCat2ToMouse === distanciaCat1ToMouse) {
     return 'os gatos trombam e o rato foge';
   }
 }
-catAndMouse(0, 3, 2);
-catAndMouse(0, 6, 12);
-catAndMouse(0, 0, 0);
+//catAndMouse(0, 3, 2);
+//catAndMouse(0, 6, 12);
+//catAndMouse();
 
 // Desafio 8
-function fizzBuzz(arrays) {
+function fizzBuzz(numeros) {
   // seu código aqui
-  let fizzBuzz = []
-  for (let i = 0; i < arrays.length; i += 1) {
-    if (arrays[i] % 3 == 0 && arrays[i] % 5 == 0) {
-      fizzBuzz.push('fizzBuzz');
-    } else if (arrays[i] % 3 == 0) {
-      fizzBuzz.push('fizz');
-    } else if (arrays[i] % 5 == 0) {
-      fizzBuzz.push('buzz');
-    } else {
-      fizzBuzz.push('bug');
+  let arrayString = [];
+  for (let indice = 0; indice < numeros.length; indice += 1) {
+    if (numeros[indice] % 3 === 0 && numeros[indice] % 5 === 0) {
+      arrayString.push('fizzBuzz');
+      //console.log('fizzBuzz');
+      continue;
     }
-
+    if (numeros[indice] % 3 === 0) {
+      arrayString.push('fizz');
+      //console.log('fizz');
+      continue;
+    }
+    if (numeros[indice] % 5 === 0) {
+      arrayString.push('buzz');
+      //console.log('buzz');
+      continue;
+    }
+    arrayString.push('bug!');
+    //console.log('bug!');
   }
-  return fizzBuzz;
+  //console.log(arrayString);
+  return arrayString;
 }
-fizzBuzz([2, 15, 7, 9, 45]);
-fizzBuzz([7, 9]);
-fizzBuzz([9, 25]);
+
+//fizzBuzz([2, 15, 7, 9, 45]);
+//fizzBuzz([7, 9]);
+//fizzBuzz([9, 25]);
 
 // Desafio 9
-function encode(string) {
+function encode(str) {
   // seu código aqui
-  const vogalEnumero = {
-    a: '1;',
-    e: '2',
-    i: '3',
-    o: '4',
-    u: '5'
-  };
-
-  let encodeString = '';
-  let x;
-
-  for (let str of string) {
-    x = str in vogalEnumero ? vogalEnumero[str] : str;
-    encodeString += x;
-  }
-
-  return encodeString;
+  let res = str.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5);
+  //console.log(res);
+  return res;
 }
-encode('hi there');
-
-console.log(encode('hi there'));
-
-function decode(string) {
+//console.log(encode('hi there'));
+//encode('hi there')
+function decode(str) {
   // seu código aqui
-  const numeroEvogal = {
-    1: 'a',
-    2: 'e',
-    3: 'i',
-    4: 'o',
-    5: 'u,'
-  };
-
-  let decodeString = '';
-  let y;
-
-  for (let str of string) {
-    y = str in numeroEvogal ? numeroEvogal[str] : str;
-    decodeString += y;
-    console.log("tchau");
-  }
-  return decodeString
+  let res = str.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  //console.log(res);
+  return res;
 }
-
+//decode('h3 th2r2')
 
 module.exports = {
   calcArea,
@@ -174,4 +151,3 @@ module.exports = {
   highestCount,
   splitSentence,
 }
-
