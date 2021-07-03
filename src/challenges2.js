000538a6742836637f00ebfdcd502727ffb5fede
danielm <play.daniel@hotmail.com>
2021-07-02 21:14:17 -0300

    Refatorando codigo do desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,23 +1,29 @@
 // Desafio 10
 function techList(lista, name) {
   let result = [];
-  for (let index = 0; index < lista.length; index += 1) {
-    let objeto = {
-      tech: '',
-      name,
-    };
-    objeto.tech = lista[index];
-    result.push(objeto);
-  }
-  result.sort(function (a, b) { // Através do sort o resultado vai obedecer o padrão de ordenação estabelecido dentro da função.
-    if (a.tech < b.tech) { // O método sorte faz uma verificação entre os elementos como se fosse um for e faz a comparação
-      return false;
-    }
-    return true;
-  });
-  if (lista.length > 0) {
-    return result;
-  } return 'Vazio!';
+	for (let index = 0; index < lista.length; index += 1) {
+		let objeto = {
+			tech: "",
+			name: name,
+		}
+		objeto.tech = lista[index];
+		result.push(objeto);
+	}
+
+	result.sort(function(a,b){  //Através do sort o resultado vai obedecer o padrão de ordenação estabelecido dentro da função.
+		if(a.tech < b.tech){      // O método sorte faz uma verificação entre os elementos como se fosse um for e faz a comparação
+			return false;                     
+		}else{
+			return true;
+		}
+	}); 
+
+	if (result.length > 0) {
+	return result;
+	} else {
+		return'Vazio!';
+	}         
+	
 }
 
 // Desafio 11
