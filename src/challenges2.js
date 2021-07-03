01a68b7e9af95a78bd4ad38c071d9cf98726e1ba
JosueTavaress <josueferreira8.jf@gmail.com>
2021-07-03 14:44:04 -0300

    adiciona Desafio 11 generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,8 +22,46 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numeroDeTelefone) {
   // seu código aqui
+
+  let mutacao,
+  numeroValidoSortt;
+mutacao = numeroDeTelefone;
+numeroValidoSortt = mutacao.slice().sort();
+ 
+  let numeroFormaTado = ["(", 0, 0, ")", " ", 0, 0, 0, 0, 0, "-", 0, 0, 0, 0];
+
+  if ( numeroDeTelefone.length !== 11 ){
+    return "Array com tamanho incorreto.";
+  }else{
+    for ( let i = 0; i < numeroDeTelefone.length; i += 1 ){
+      if (numeroDeTelefone[i] < 0 || numeroDeTelefone[i] > 9){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+  }
+
+  for (let i = 0; i < 11; i += 1){
+    if (numeroValidoSortt[i] === numeroValidoSortt[i + 1] && numeroValidoSortt[ i + 2] === numeroValidoSortt[i]){
+      return "não é possível gerar um número de telefone com esses valores";
+  }
+}
+
+numeroFormaTado[1] = numeroDeTelefone[0];
+numeroFormaTado[2] = numeroDeTelefone[1];
+numeroFormaTado[5] = numeroDeTelefone[2];
+
+  for (let y = 6; y < 10; y += 1 ){
+      numeroFormaTado[y] = numeroDeTelefone[ y - 3 ];
+  }
+
+  for (let y = 11; y < 15; y += 1 ){
+    numeroFormaTado[y] = numeroDeTelefone[ y - 4 ];
+  }
+
+    let resultado = numeroFormaTado.join('');
+    return resultado;
 }
 
 // Desafio 12
