96611592c7dd3b545bad4369b59ef961d678eaea
Carlos Afonso <carlos.flach15@gmail.com>
2021-07-03 22:50:18 -0300

    Melhorando desafio 10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,30 +1,44 @@
 // Desafio 10
 function techList(techList,name) {
   // seu código aqui
-  let listaOrdenados =[]; // array que vai receber os objetos.
+  let listaOrdenados; // variavel que vai receber os objetos.
   techList.sort(); // ordenar as informações do array recebido.
-  for(let index in techList) {
-    // Adiciona os objetos em ordem para o novo array.
-    listaOrdenados.push({
-      tech: techList[index],
-      name: name,
-    }) 
-  }
-  // Verifica se é vazio ou não o array de entrada.
+  // verifica se é vazio.
   if (techList.length === 0){
     listaOrdenados = 'Vazio!';
-    return listaOrdenados; // retorna com a informaçõ de vazio!
   }
-  else {
-    return listaOrdenados; // retorna com os objetos.
+  else{
+    listaOrdenados = [];
+    for (let index in techList){
+      listaOrdenados.push({
+        tech: techList[index],
+        name: name,
+      })
+    }
   }
+  return listaOrdenados
 }
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numeros) {
   // seu código aqui
+//   let retorno;
+//   let contador = 0;
+//   // se receber um array com tamanho diferente de 11, retorna "Array com tamanho incorreto".
+//   if(numeros.length != 11){
+//     retorno = 'Array com tamanho incorreto.'
+//   }
+//   // verifica se é menor que zero ou maior que 9
+//   for (let index in numeros) {
+//     if (numeros[index] < 0 || numeros[index] > 9){
+//       retorno = "não é possível gerar um número de telefone com esses valores."
+//     } 
+//   }
+
+//  return retorno;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
