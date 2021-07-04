659235a356e535f94ee875d8b95435c173f5864a
Carlos Afonso <carlos.flach15@gmail.com>
2021-07-03 22:26:54 -0300

    Desafio 10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,25 @@
 // Desafio 10
-function techList() {
+function techList(techList,name) {
   // seu código aqui
+  let listaOrdenados =[]; // array que vai receber os objetos.
+  techList.sort(); // ordenar as informações do array recebido.
+  for(let index in techList) {
+    // Adiciona os objetos em ordem para o novo array.
+    listaOrdenados.push({
+      tech: techList[index],
+      name: name,
+    }) 
+  }
+  // Verifica se é vazio ou não o array de entrada.
+  if (techList.length === 0){
+    listaOrdenados = 'Vazio!';
+    return listaOrdenados; // retorna com a informaçõ de vazio!
+  }
+  else {
+    return listaOrdenados; // retorna com os objetos.
+  }
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
