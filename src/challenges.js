85596dee2b336e80f4abf000dc7ae8e62b7b7fc8
Yan Brasileiro <yan.brasileiro@gmail.com>
2021-07-04 18:58:47 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -102,11 +102,49 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+frase = string;
+
+  for (let i = 0; i < frase.length; i+= 1){
+    if(frase[i] === "a"){
+      frase = frase.replace("a", 1);    
+    }else if (frase[i] === "e"){
+     frase = frase.replace("e", 2);   
+  }else if (frase[i] === "i"){
+   frase = frase.replace("i", 3);
+  }else if (frase[i] === "o"){
+   frase = frase.replace("o", 4);
+  }else if (frase[i] === "u"){
+   frase = frase.replace("u", 5);
+  }
+
 }
-function decode() {
+return frase;
+
+}
+
+function decode(string) {
   // seu código aqui
+  frase = string;
+
+  for (let i = 0; i < frase.length; i+= 1){
+    if(frase[i] === "1"){
+     frase = frase.replace("1", "a");    
+    }else if (frase[i] === "2"){
+      frase = frase.replace("2", "e");   
+    }else if (frase[i] === "3"){
+     frase = frase.replace("3", "i");
+    }else if (frase[i] === "4"){
+     frase = frase.replace("4", "o");
+    }else if (frase[i] === "5"){
+     frase = frase.replace("5", "u");
+    }
+  
+  }
+  
+return frase;
+
 }
 
 module.exports = {
@@ -120,4 +158,4 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
-};
+}
