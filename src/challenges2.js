6612c01d881a2349e46def812c6f68c2b5263cc5
Carlos Afonso <carlos.flach15@gmail.com>
2021-07-04 00:09:00 -0300

    Desafio 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,15 +1,14 @@
 // Desafio 10
-function techList(techList,name) {
+function techList(techList, name) {
   // seu código aqui
   let listaOrdenados; // variavel que vai receber os objetos.
   techList.sort(); // ordenar as informações do array recebido.
   // verifica se é vazio.
-  if (techList.length === 0){
+  if (techList.length === 0) {
     listaOrdenados = 'Vazio!';
-  }
-  else{
+  } else {
     listaOrdenados = [];
-    for (let index in techList){
+    for (let index in techList) {
       listaOrdenados.push({
         tech: techList[index],
         name: name,
@@ -18,41 +17,63 @@ function techList(techList,name) {
   }
   return listaOrdenados
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
+// console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
   // seu código aqui
-//   let retorno;
-//   let contador = 0;
-//   // se receber um array com tamanho diferente de 11, retorna "Array com tamanho incorreto".
-//   if(numeros.length != 11){
-//     retorno = 'Array com tamanho incorreto.'
-//   }
-//   // verifica se é menor que zero ou maior que 9
-//   for (let index in numeros) {
-//     if (numeros[index] < 0 || numeros[index] > 9){
-//       retorno = "não é possível gerar um número de telefone com esses valores."
-//     } 
-//   }
+  let retorno = '('
+  // Verifica se o array.length é maior que 11.
+  if (numeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  // verifica se algum número é menor que zero ou maior que nove.
+  for (index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] < 0 || numeros[index] > 9 || numeros[index] < 0 && numeros[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  // verifica se tem algum número que se repete mais que 3 vezes.
+  for (let n in numeros) {
+    let cont = 0;
+    for (let m in numeros) {
+      if (numeros[m] === numeros[n]) {
+        cont += 1;
+      }
+    }
+    if (cont >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
 
-//  return retorno;
-}
+  for (let idx in numeros) {
+    if (idx == 2) {
+        retorno += ') '
+        retorno += numeros[idx];
+    } else if (idx == 7) {
+        retorno += '-';
+        retorno += numeros[idx];
+    } else {
+        retorno += numeros[idx]
+    }
+  }
+return retorno;
+ }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+  // Desafio 12
+  function triangleCheck() {
+    // seu código aqui
+  }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+  // Desafio 13
+  function hydrate() {
+    // seu código aqui
+  }
 
-module.exports = {
-  generatePhoneNumber,
-  techList,
-  hydrate,
-  triangleCheck,
-};
+  module.exports = {
+    generatePhoneNumber,
+    techList,
+    hydrate,
+    triangleCheck,
+  };
