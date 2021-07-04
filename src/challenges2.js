4e0dc39ef6b7897fbc99d8372a41998c098d904d
DanFilipe2000 <anielilipe90@gmail.com>
2021-07-03 22:15:14 -0300

    resolve exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+    let lista = tech.sort();
+    let objt = [];
+
+    if(lista.length === 0){
+      return "Vazio!";
+    }else if(lista.length > 0){
+      for(let i = 0; i < lista.length; i+=1){
+        let lista1 = {
+          tech: lista[i],
+          name: name
+        };
+        objt.push(lista1);
+      }
+    }
+  return objt;
 }
 
 // Desafio 11
