c24104f2a4448d3f6c0d1d92d16e4307be90013c
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-07-05 03:03:22 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,9 +17,50 @@ function techList(tech, name) {
 console.log(techList(["React", "Bootstrap", "Typescript", "Angular.js", "JavaScript"], "Rabito" ))
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatenumTelephone(telefone) {
+  let numTelephone = '(';
+  if  (telefone.length != 11) {
+    return "Array com tamanho incorreto.";
+  } else {
+    for (let i = 0; i < telefone.length; i += 1) {
+      let contador = 0;
+      if(telefone[1] < 0 || telefone[1] > 9){
+        return " não é possível gerar um número de telefone com esses valores";
+      }
+      for (let j = 0; j < 11; j += 1) {
+        if(telefone[i] === telefone[j]) {
+          contador += 1;
+          if (contador === 3) {
+            return "não é possível gerar um número de telefone com esses valores";
+          }
+        }
+      }
+      if (i < 2) {
+        numTelephone = numTelephone + telefone[i]; 
+        if (i === 1) {
+          numTelephone = numTelephone + ') ';
+        } 
+      }
+      if (i <= 6 && i > 1) {
+        numTelephone = numTelephone + telefone[i];
+        if(i === 6){
+          numTelephone = numTelephone + '-';
+        }
+      }
+      if (i > 6) {
+        numTelephone = numTelephone + telefone[i];
+    }
+    
+    
+    }
+    
+  return numTelephone;
+  }
+  
 }
+console.log(generatenumTelephone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+
+
 
 // Desafio 12
 function triangleCheck() {
@@ -32,7 +73,7 @@ function hydrate() {
 }
 
 module.exports = {
-  generatePhoneNumber,
+  generatenumTelephone,
   techList,
   hydrate,
   triangleCheck,
