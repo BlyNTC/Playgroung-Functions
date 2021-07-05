41ca1bbb0a9269b8bb9068ca083c87e5869b6179
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-07-05 13:21:57 -0300

    arrumando todos os erros dos exercicios

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,23 +15,24 @@ function techList(tecnologia, name) {
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
-  let quantidadeRepeticao = 0;
-  if (numeros !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  for (let valor1 of numeros) {
-    for (let valor2 of numeros) {
-      if (valor1 === valor2) {
-        quantidadeRepeticao += 1;
-      }
-    }
-    if (valor1 < 0 || valor1 > 9 || quantidadeRepeticao >= 3) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-    quantidadeRepeticao = 0;
-  }
+//   let quantidadeRepeticao = 0;
+//   if (numeros !== 11) {
+//     return 'Array com tamanho incorreto.';
+//   }
+//   for (let valor1 of numeros) {
+//     for (let valor2 of numeros) {
+//       if (valor1 === valor2) {
+//         quantidadeRepeticao += 1;
+//       }
+//     }
+//     break;
+//     if (valor1 < 0 || valor1 > 9 || quantidadeRepeticao >= 3) {
+//       return 'não é possível gerar um número de telefone com esses valores';
+//     }
+//     quantidadeRepeticao = 0;
+//   }
 }
-console.log();
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -47,19 +48,20 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(string) {
   let retornaString = '';
-  let totalNumero = string.match(/\d+/g);
-  if (totalNumero == 1) {
-    retornaString = retornaString.concat(totalNumero, ' copo de água');
-  }
+  let contagem = 0;
+  let totalNumero = string.match(/\d+/g).map(Number);
   for (let i = 0; i < totalNumero.length; i += 1) {
     if (totalNumero[i] >= 1) {
-      totalNumero += totalNumero[i];
-      retornaString = retornaString.concat(totalNumero, ' copos de água');
+      contagem += totalNumero[i];
     }
+  }
+  if (contagem === 1) {
+    retornaString = retornaString.concat(contagem, ' copo de água');
+  } else if (contagem > 1) {
+    retornaString = retornaString.concat(contagem, ' copos de água');
   }
   return retornaString;
 }
-console.log(hydrate('1 cerveja e 4 cerveja'));
 
 module.exports = {
   generatePhoneNumber,
