969c8edcf5f42ed126d301af34a04fb8002ee806
Carlos Afonso <carlos.flach15@gmail.com>
2021-07-04 22:14:15 -0300

    Adicionando variavel de armazenamento no desafio 13 e melhorando o lint.

--- src/challenges2.js
+++ src/challenges2.js
@@ -58,50 +58,50 @@ function generatePhoneNumber(numeros) {
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck(lineA,lineB,lineC) {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
   let status; // variável que vai armazenar o valor booleano.
   let verificaA = false;
   let verificaB = false;
   let verificaC = false;
-  if(lineA > Math.abs(lineB-lineC) && lineA < lineB + lineC) {
+  if (lineA > Math.abs(lineB - lineC) && lineA < lineB + lineC) {
     verificaA = true;
-  } 
-  if (lineB > Math.abs(lineA-lineC) && lineB < lineA + lineC) {
+  }
+  if (lineB > Math.abs(lineA - lineC) && lineB < lineA + lineC) {
     verificaB = true;
   }
-  if (lineC > Math.abs(lineA-lineB) && lineC < lineA + lineB) {
+  if (lineC > Math.abs(lineA - lineB) && lineC < lineA + lineB) {
     verificaC = true;
   }
-  if (verificaA === true && verificaB === true && verificaC === true){
+  if (verificaA === true && verificaB === true && verificaC === true) {
     status = true;
   } else {
     status = false;
   }
-  return status
+  return status;
 }
 console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate(string) {
   // seu código aqui
-let reg = /\d+/g;
-let result = string.match(reg);
-let array = [];
-let cont = 0;
-let saida;
-for (let index = 0; index < result.length; index += 1) {
-  array.push(parseInt(result[index]));
-}
-for (let i = 0; i < array.length; i += 1){
-  cont += array[i];
-}
-if (cont > 1) {
-  saida = cont + ' copos de água';
-} else {
-  saida = cont + ' copo de água';
-}
-return saida;
+  let reg = /\d+/g;
+  let result = string.match(reg);
+  let array = [];
+  let cont = 0;
+  let saida;
+  for (let index = 0; index < result.length; index += 1) {
+    array.push(parseInt(result[index]));
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    cont += array[i];
+  }
+  if (cont > 1) {
+    saida = cont + ' copos de água';
+  } else {
+    saida = cont + ' copo de água';
+  }
+  return saida;
 }
 console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
