8cf7a180dedbfe787d97373fc9a4e37f5b5484bd
Carlos Afonso <carlos.flach15@gmail.com>
2021-07-04 22:30:30 -0300

    Finaliza projeto.

--- src/challenges2.js
+++ src/challenges2.js
@@ -60,7 +60,7 @@ console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  let status; // variável que vai armazenar o valor booleano.
+  let status; 
   let verificaA = false;
   let verificaB = false;
   let verificaC = false;
