9901c1b6812a4b39bffb340b8efff1b7a69e8672
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-07-05 18:26:35 -0300

    Desafio completos

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,7 +42,7 @@ function generatenumTelephone(digitosFoneVetor) {
   return numFoneCompleto;
 }
 
-console.log(generatenumTelephone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+generatenumTelephone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 
 
