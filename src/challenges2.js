ce8997ff904097f64fcc16c33e402ba1135bb61e
Pedro Veiga <pedroveiga.ti@gmail.com>
2021-07-05 18:34:35 -0300

    Desafio completos

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,6 +17,7 @@ function techList(tech, name) {
 techList(['React', 'Bootstrap', 'Typescript', 'Angular.js', 'JavaScript'], 'Rabito');
 
 // Desafio 11
+
 function generatenumTelephone(digitosFoneVetor) {
   let vezesRepet = 0;
   let vetorDDD = [digitosFoneVetor[0]]+ [digitosFoneVetor[1]];
@@ -42,7 +43,7 @@ function generatenumTelephone(digitosFoneVetor) {
   return numFoneCompleto;
 }
 
-generatenumTelephone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+// generatenumTelephone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 
 
