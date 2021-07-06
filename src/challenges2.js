6d4ae6603533ddb572e52f3535ef82c90cb06547
Victor hugo Souza <victoraster2014@gmail.com>
2021-07-06 10:01:10 -0300

    Inclusao do desafio de lista

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(technologies, name)
+ {  //seu código aqui
+  let listaObjetos = [];
+    if (technologies.length === 0) {
+      return 'Vazio!';
+    }
+  for (let tech of technologies.sort()) {
+      listaObjetos.push({
+        tech,
+        name,
+      });
+  }
+    return listaObjetos;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
