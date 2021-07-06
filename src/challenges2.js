225faad2a57e441a2e2faf738651600a3a499b28
Fernando Monteiro <fernandoflmbatera@gmail.com>
2021-07-06 12:31:50 -0300

    Desafio 10 e 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,14 +1,41 @@
 // Desafio 10
 function techList() {
   // seu código aqui
+  let result = [];
+	if (techName.lenght === 0) {
+		return 'Vazio!'
+	}
+	for (let tech of techName.sort()) {
+		result.push({tech, name})
+	}
+	return result;
 }
 
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
+  let sameAsNumber = number
+	let filteredNumbers = number.filter(sameAsNumber);
+		
 
+if (filteredNumbers.length === 11 && filteredNumbers.length) {
+		let telNumber =  '(' + number.slice(0, -9) + ')' + number.slice(2, -4) + '-' + number.slice(7);
+		let result = telNumber.replaceAll(',', '');
+		return result;
 
-  // Fernando: pesquisar sobre slice para essa
+	} else if (filteredNumbers.length < 11) {
+		return 'Array com tamanho errado';
+	} else if (filteredNumbers < 0 && fileredNumbers > 9) {
+	} else {
+		return 'lascou';
+	}
+
+	// Referências:
+	
+	// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+	// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+	// https://stackoverflow.com/questions/7806709/remove-trailing-comma-from-comma-separated-string/23382913
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 }
 
 // Desafio 12
