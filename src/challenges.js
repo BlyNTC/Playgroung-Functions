ec91ee9fa22af29d9ac12e70843d8f5e96b9e4ce
GabrielAlex99 <galexandersantos975@gmail.com>
2021-07-27 13:52:33 -0300

    teste

--- src/challenges.js
+++ src/challenges.js
@@ -1,27 +1,27 @@
 // Desafio 1
 function compareTrue(param1, param2) {
   return param1 && param2;
-}
+};
 
 // Desafio 2
 function calcArea(base, height) {
   let area = (base * height) / 2;
   return area;
-}
+};
 //console.log(calcArea(5,2));
 
 // Desafio 3
 function splitSentence(funcao) {
   let resultado = funcao.split(' ');
   return resultado;
-}
+};
 
 // Desafio 4
 function concatName(nomes) {
   let first = nomes[0];
   let last = nomes[nomes.length - 1];
   return (last + ', ' + first);
-}
+};
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -38,7 +38,7 @@ function footballPoints(wins, ties) {
   }
   pontos += vitoria + empate;
   return(pontos);
-} //footballPoints(1, 2);
+}; //footballPoints(1, 2);
 
 // Desafio 6
 function highestCount(numbers) {
@@ -58,9 +58,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanciaCat1 = Math.abs(mouse - cat1);
   let distanciaCat2 = Math.abs(mouse - cat2);
   let resultado = '';
-  if (cat1 < cat2) {
+  if (distanciaCat1 < distanciaCat2) {
     resultado = 'cat1';
-  } else if (cat2 < cat1) {
+  } else if (distanciaCat2 < distanciaCat1) {
     resultado = 'cat2';
   } else {
     resultado = ('os gatos trombam e o rato foge');
@@ -69,24 +69,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }; //console.log(catAndMouse(0, 1, 1));
 
 // // Desafio 8
-function fizzBuzz() {
-  let numbers = [];
-  let divisivel;
-  let tipo;
-  let tiposString = [];
-  for (var i = 0; i < numbers.length; i++) {
-    divisivel = numbers[i];
-    if (divisivel % 3 === 0 && divisivel % 5 === 0) {
-      tipo = "fizzBuzz";
-    } else if (divisivel % 3 === 0) {
-      tipo = "fizz";
-    } else if (divisivel % 5 === 0) {
-      tipo = "buzz";
+function fizzBuzz(numbers) {
+  let tipo = [];
+  for (let num of numbers) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      tipo.push("fizzBuzz");
+    } else if (num % 3 === 0) {
+      tipo.push("fizz");
+    } else if (num % 5 === 0) {
+      tipo.push("buzz");
     } else {
-      tipo = "bugg!";
+      tipo.push("bug!");
     }
   }
-  return tiposString;
+  return tipo;
 };
 
 // Desafio 9
