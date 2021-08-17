027fab9016f1cf928bc1b526f087db9113793760
Maurício Leite <mauricioleite1@gmail.com>
2021-08-17 10:29:16 -0300

    Refatorando funções do arquivo challenges.js

--- src/challenges.js
+++ src/challenges.js
@@ -1,92 +1,59 @@
 // Desafio 1
-
-function compareTrue(a1, a2) {
-  if (a1 === true && a2 === true) {
-    return true;
-  } else {
-    return false;
-  }
-}
+const compareTrue = (a1, a2) => a1 && a2;
 
 // Desafio 2
-function calcArea(b, h) {
-  return (b * h) / 2;
-}
+const calcArea = (b, h) => (b * h) / 2;
 
 // Desafio 3
-function splitSentence(arraySentence) {
-  return arraySentence.split(' ');
-}
+const splitSentence = (arraySentence) => arraySentence.split(' ');
 
 // Desafio 4
-function concatName(arSentence) {
-  let lastItem = arSentence[arSentence.length - 1];
-  let firstItem = arSentence[0];
-  let concat = lastItem + ', ' + firstItem;
-  return concat;
-}
+const concatName = (arSentence) => `${arSentence[arSentence.length - 1]}, ${arSentence[0]}`;
 
 // Desafio 5
-function footballPoints(wins, ties) {
-  let win = wins * 3;
-  return win + ties;
-}
+const footballPoints = (wins, ties) => (wins * 3) + ties;
 
 // Desafio 6
-function highestCount(ar) {
-  let highestValue = ar[0];
-  let count = 0;
-  for (let i = 0; i < ar.length; i += 1) {
-    if (ar[i] >= highestValue) {
-      if (ar[i] !== highestValue) {
-        highestValue = ar[i];
-        count = 0;
-      }
-      count += 1;
+const highestCount = (ar) => {
+  let highest = ar[0];
+  return ar.reduce((a, v) => {
+    if (v === highest) {
+      a += 1;
+      return a;
     }
-  }
-  return count;
-}
+    if (v > highest) {
+      highest = v;
+      a = 1;
+    }
+    return a;
+  }, highest);
+};
 
 // Desafio 7
-function catAndMouse(cat1, cat2) {
-  if (cat1 > cat2) {
-    return 'cat2';
-  } if (cat1 < cat2) {
-    return 'cat1';
-  } else {
-    return 'os gatos trombam e o rato foge';
-  }
-}
+const catAndMouse = (mouse, c1, c2) => {
+  if (Math.abs(mouse - c1) < Math.abs(mouse - c2)) { return 'cat1'; }
+  if (Math.abs(mouse - c1) > Math.abs(mouse - c2)) { return 'cat2'; }
+  if (Math.abs(mouse - c1) === Math.abs(mouse - c2)) { return 'os gatos trombam e o rato foge'; }
+};
 
 // Desafio 8
-function fizzBuzz(fbA) {
-  for (let i = 0; i < fbA.length; i += 1) {
-    if (fbA[i] % 3 === 0 && fbA[i] % 5 === 0) {
-      fbA[i] = 'fizzBuzz';
-    } else if (fbA[i] % 5 === 0) {
-      fbA[i] = 'buzz';
-    } else if (fbA[i] % 3 === 0) {
-      fbA[i] = 'fizz';
-    } else {
-      fbA[i] = 'bug!';
-    }
-  }
-  return fbA;
-}
+const fizzBuzz = (fbA) => fbA.map((a) => {
+  if (a % 3 === 0 && a % 5 === 0) { return 'fizzBuzz'; }
+  if (a % 3 === 0) { return 'fizz'; }
+  if (a % 5 === 0) { return 'buzz'; }
+  return 'bug!';
+});
 
 // Desafio 9
-function encode(fraseE) {
-  let letrasEncode = { 'a':'1', 'e':'2', 'i':'3', 'o':'4', 'u':'5'};
-  fraseE = fraseE.replace(/[aeiou]/g, m => letrasEncode[m]);
-  return fraseE;
-}
+const encode = (fraseE) => {
+  let letras = { a: '1', e: '2', i: '3', o: '4', u: '5' };
+  return fraseE.replace(/[aeiou]/g, (m) => letras[m]);
+};
 
-function decode(fraseD) {
-  let letrasDecode = { '1':'a', '2':'e', '3':'i', '4':'o', '5':'u' };
-  fraseD = fraseD.replace(/[12345]/g, m => letrasDecode[m]);
-  return fraseD;
-}
+const decode = (fraseD) => {
+  let letras = { 1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u' };
+  return fraseD.replace(/[12345]/g, (m) => letras[m]);
+};
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -18,9 +18,25 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  let phoneNumber = [7, 1, 9, 8, 8, 1, 1, 1, 1, 0, 8]
+
+  console.log(phoneNumber.indexOf(7));
+
+  if (phoneNumber.length === 11) {
+
+    for (let i = 0; i < phoneNumber.length; i += 1) {
+      if (phoneNumber[i] < 0 || phoneNumber[i] > 9 || )
+    }
+  } else {
+    return 'Array com tamanho incorreto.'
+  }
 }
+
+// let uniqueCount = ["a","b","c","d","d","e","a","b","c","f","g","h","h","h","e","a"];
+// let count = {};
+// uniqueCount.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+// console.log(count);
 
 // Desafio 12
 function triangleCheck() {
