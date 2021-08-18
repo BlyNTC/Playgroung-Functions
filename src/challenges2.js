96eb947e2af14a45fadcbe87347872a6fbe3984f
Samuel Nascimento <sfsnascimento@gmail.com>
2021-08-18 11:52:46 -0300

    refatorando requisito 1

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,26 +19,25 @@ function techList(array, name) {
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
-//Calcula o numero de repetição e retorna se o número for superado
-    let vezesRepetido = 0;
-    for (let index = 0; index < numeros.length; index += 1) {
-      for (let i = 0; i < numeros.length; i += 1) {
-        if(numeros[i] === numeros[index]) {
+// Calcula o numero de repetição e retorna se o número for superado
+  let vezesRepetido = 0;
+  for (let index = 0; index < numeros.length; index += 1) {
+    for (let i = 0; i < numeros.length; i += 1) {
+      if (numeros[i] === numeros[index]) {
         vezesRepetido += 1;
-        }
-        if(vezesRepetido >= 3){
-          return 'não é possível gerar um número de telefone com esses valores';
-        }
       }
-      vezesRepetido = 0;
+      if (vezesRepetido >= 3){
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
     }
-//Faz os outros testes
+    vezesRepetido = 0;
+  }
+// Faz os outros testes
   if (numeros.length !== 11){
     return 'Array com tamanho incorreto.';
   }
   let numeroTelefone = '(';
   for (let index = 0; index < numeros.length; index += 1) {
-    
     if (numeros[index] < 0 || numeros[index] > 9) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
@@ -48,11 +47,10 @@ function generatePhoneNumber(numeros) {
       numeroTelefone += '-';
     } 
     numeroTelefone += numeros[index];
-    
   }
   return numeroTelefone;
 }
-//console.log(generatePhoneNumber([1, 1, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (Math.abs(lineB - lineC) < lineA && lineA < (lineB + lineC) && Math.abs(lineA - lineC) < lineB && lineB < (lineA + lineC) && Math.abs(lineA - lineB) < lineC && lineC < (lineA + lineB)) {
