8594c827c24d88bc1ceac183353a0b12fc586f97
brunofeu <brunofeu89@gmail.com>
2021-08-26 15:09:22 -0300

    refatorando até requisito 13 e ajustando lint para finalizar

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,26 +5,33 @@ const techList = (array, name) => {
 };
 
 // Desafio 11
-const generatePhoneNumber = (a) => {
-  if (a.length !== 11) return 'Array com tamanho incorreto.';
-  if (a.find((number) => number < 0 || number > 9)
+const checkValidNumber = (phoneNumber) => {
+  if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
+  if (phoneNumber.find((number) => number < 0 || number > 9)
   ) return 'não é possível gerar um número de telefone com esses valores';
-  for (let numberToCompare of a) {
-    if (a.filter((number) => number === numberToCompare).length
-      >= 3) return 'não é possível gerar um número de telefone com esses valores';
+};
+
+const checkRepeatNumber = (phoneNumber) => {
+  for (let compareNumber of phoneNumber) {
+    if (phoneNumber.filter((number) => number === compareNumber).length
+    >= 3) return 'não é possível gerar um número de telefone com esses valores';
   }
-  return `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
+};
+
+const generatePhoneNumber = (n) => {
+  if (checkValidNumber(n)) return checkValidNumber(n);
+  if (checkRepeatNumber(n)) return checkRepeatNumber(n);
+  return `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`;
 };
 
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC) {
-  if (lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineA + lineB) {
-    return false;
-  } if (lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineB - lineA)) {
-    return false;
-  }
-  return true;
-}
+
+const compareLineToSum = (a, b, c) => !(a > b + c || b > a + c || c > a + b);
+const compareLineToSub = (a, b, c) =>
+  !(a < Math.abs(b - c) || b < Math.abs(a - c) || c < Math.abs(b - a));
+
+const triangleCheck = (lineA, lineB, lineC) =>
+  compareLineToSum(lineA, lineB, lineC) && compareLineToSub(lineA, lineB, lineC);
 
 // Desafio 13
 function hydrate(string) {
@@ -33,12 +40,9 @@ function hydrate(string) {
  * https://www.youtube.com/watch?v=pfkkdzeyx6U
  */
   let waterString = string.match(/\d+/g);
-  let waterNumber = 0;
-  for (let index in waterString) waterNumber += parseInt(waterString[index]);
-  if (waterNumber > 1) {
-    return `${waterNumber} copos de água`;
-  }
-  return `${waterNumber} copo de água`;
+  let waterNumber = waterString.reduce((acc, quant) => acc + parseInt(quant, 10), 0);
+  const moreThanOne = waterNumber > 1 ? 's' : '';
+  return `${waterNumber} copo${moreThanOne} de água`;
 }
 
 module.exports = {
